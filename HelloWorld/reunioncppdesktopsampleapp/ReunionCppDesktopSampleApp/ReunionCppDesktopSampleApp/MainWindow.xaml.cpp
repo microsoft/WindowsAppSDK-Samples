23a5c0e90a0773adf3ac5894c418c431a8224e62
Eli Payzer <eliezerp@microsoft.com>
2020-12-03 15:05:35 -0800

    Initial commit of Hello World app
    This is the initial commit of the Hello World WinUI3 Win32 packaged app that demonstrates pulling in the Project Reunion Nuget and building with it.

---
+++ HelloWorld/reunioncppdesktopsampleapp/ReunionCppDesktopSampleApp/ReunionCppDesktopSampleApp/MainWindow.xaml.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::ReunionCppDesktopSampleApp::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        // Call something exposed through Project Reunion as a sanity test that things are loading correctly.
+        // When more is added to Project Reunion there will be more relevant sample content.
+        auto activationArgs = winrt::Microsoft::ProjectReunion::AppLifecycle::GetActivatedEventArgs();
+
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
