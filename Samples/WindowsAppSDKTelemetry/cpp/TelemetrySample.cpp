34b1cc4b874a8c919ec5f5d841464a0c07cd8be9
Chandra Srinivasan <chandra.srinivasan@outlook.com>
2021-07-27 17:07:46 -0700

    Adding CPP sample for apps to use the WindowsAppSDK telemetry framework

--- Samples/WindowsAppSDKTelemetry/cpp/TelemetrySample.cpp
+++ Samples/WindowsAppSDKTelemetry/cpp/TelemetrySample.cpp
@@ -1,7 +1,6 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
 
-#include <iostream>
 #include "TelemetrySample.h"
 
 int main()
@@ -10,14 +9,3 @@ int main()
     WindowsAppSDKTelemetrySample::TextPayloadEvent(L"This text is the event payload");
     WindowsAppSDKTelemetrySample::EventWithUserDefinedType(L"This is the use-defined type payload");
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
