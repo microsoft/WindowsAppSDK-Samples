8dee27190e4ac51ecb150271e410fc364540694d
Eve <evmwangi@microsoft.com>
2021-08-20 11:48:46 -0700

    double click/tap of custom window icon closes the window

--- Samples/Windowing/cpp-winui/SampleApp/MainWindow.xaml.h
+++ Samples/Windowing/cpp-winui/SampleApp/MainWindow.xaml.h
@@ -16,12 +16,13 @@ public:
     void NavigationView_Loaded(winrt::IInspectable const& sender, winrt::RoutedEventArgs const& args);
     void NavigationView_ItemInvoked(winrt::IInspectable const& sender, winrt::NavigationViewItemInvokedEventArgs const& args);
     void NavigationView_BackRequested(winrt::NavigationView const& sender, winrt::NavigationViewBackRequestedEventArgs const& args);
+    void MyWindowIcon_DoubleTapped(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::DoubleTappedRoutedEventArgs const& e);
 
 private:
     winrt::AppWindow GetAppWindowForCurrentWindow();
 
     winrt::AppWindow m_mainAppWindow{ nullptr };
-    hstring m_windowTitle = L"WinUI Desktop C++ Sample App";
+    hstring m_windowTitle = L"WinUI Desktop C++ Sample App";  
 };
 
 }
