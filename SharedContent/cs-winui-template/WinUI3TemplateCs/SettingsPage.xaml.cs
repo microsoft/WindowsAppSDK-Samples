3dd5c76433aa919a7def96837f85825222c32c03
Angela Zhang <angzhang@microsoft.com>
2021-09-14 12:37:41 -0700

    formatting

--- SharedContent/cs-winui-template/WinUI3TemplateCs/SettingsPage.xaml.cs
+++ SharedContent/cs-winui-template/WinUI3TemplateCs/SettingsPage.xaml.cs
@@ -12,9 +12,7 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
-using System;
 using System.Linq;
-using System.Reflection;
 
 namespace WinUI3TemplateCs
 {
