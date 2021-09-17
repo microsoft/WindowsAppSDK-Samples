cc9979e6f803bccd1b10003e3b810934731a2dfb
Erik Torres <ertorres@microsoft.com>
2021-09-17 12:52:08 -0700

    update cs-winforms-unpackaged nugets

--- Samples/ResourceManagement/cs-winforms-unpackaged/Program.cs
+++ Samples/ResourceManagement/cs-winforms-unpackaged/Program.cs
@@ -3,7 +3,7 @@
 
 using System;
 using System.Windows.Forms;
-using Microsoft.ApplicationModel.Resources;
+using Microsoft.Windows.ApplicationModel.Resources;
 using Microsoft.Windows.ApplicationModel;
 
 namespace winforms_unpackaged_app
