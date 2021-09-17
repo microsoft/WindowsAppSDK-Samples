aab5f7bb5723b9a7f73e6e1942b3fe81abaa88d7
Erik Torres <ertorres@microsoft.com>
2021-09-17 12:57:34 -0700

    update cs-winui nuget packages

--- Samples/ResourceManagement/cs-winui/winui_desktop_packaged_app/MainWindow.xaml.cs
+++ Samples/ResourceManagement/cs-winui/winui_desktop_packaged_app/MainWindow.xaml.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Microsoft.UI.Xaml;
-using Microsoft.ApplicationModel.Resources;
+using Microsoft.Windows.ApplicationModel.Resources;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
