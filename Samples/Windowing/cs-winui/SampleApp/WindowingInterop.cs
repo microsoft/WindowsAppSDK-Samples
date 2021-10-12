bfb2c98ef40f8114cf5c14bba9afeb12195d81b2
Roberth Karman <roberth.karman@microsoft.com>
2021-10-12 11:48:20 +0200

    Preview2 changes
    Updated to Preview 2 nuget package.
    Updated Interop class to not use DLLImport.

--- Samples/Windowing/cs-winui/SampleApp/WindowingInterop.cs
+++ Samples/Windowing/cs-winui/SampleApp/WindowingInterop.cs
@@ -6,16 +6,6 @@ using WinRT;
 
 namespace SampleApp
 {
-    internal class Interop
-    {
-        [DllImport("Microsoft.Internal.FrameworkUdk.dll", EntryPoint = "Windowing_GetWindowFromWindowId", CharSet = CharSet.Unicode)]
-        public static extern IntPtr GetWindowFromWindowId(WindowId windowId, out IntPtr result);
-
-
-        [DllImport("Microsoft.Internal.FrameworkUdk.dll", EntryPoint = "Windowing_GetWindowIdFromWindow", CharSet = CharSet.Unicode)]
-        public static extern IntPtr GetWindowIdFromWindow(IntPtr hwnd, out WindowId result);
-    }
-
     public static class AppWindowExtensions
     {
         public static AppWindow GetAppWindow(this Microsoft.UI.Xaml.Window window)
@@ -27,12 +17,7 @@ namespace SampleApp
 
         private static AppWindow GetAppWindowFromWindowHandle(IntPtr windowHandle)
         {
-            WindowId windowId;
-
-            Interop.GetWindowIdFromWindow(windowHandle, out windowId);
-
-            return AppWindow.GetFromWindowId(windowId);
+            return AppWindow.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(windowHandle));
         }
-
     }
 }
