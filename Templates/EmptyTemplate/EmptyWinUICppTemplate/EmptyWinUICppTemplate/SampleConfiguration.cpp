9321ed09ed6ba9efb7ca0ebe6e84f9270c1cc240
Angela Zhang <angzhang@microsoft.com>
2021-10-15 10:07:46 -0700

    add empty templates from generated vsix

---
+++ Templates/EmptyTemplate/EmptyWinUICppTemplate/EmptyWinUICppTemplate/SampleConfiguration.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+#include "pch.h"
+#include "SampleConfiguration.h"
+#include "MainPage.xaml.h" 
+
+using namespace winrt;
+using namespace winrt::EmptyWinUICppTemplate;
+using namespace Microsoft::UI::Xaml;
+using namespace Windows::Foundation::Collections;
+
+IVector<Scenario> implementation::MainPage::scenariosInner = winrt::single_threaded_observable_vector<Scenario>(
+	{
+		Scenario{ L"Scenario 1", winrt::hstring(winrt::name_of<EmptyWinUICppTemplate::Scenario1_ShortName>())},
+		Scenario{ L"Scenario 2", winrt::hstring(winrt::name_of<EmptyWinUICppTemplate::Scenario2_ShortName>())}
+	});
+
+hstring SampleConfig::FeatureName{ L"EmptyWinUICppTemplate" };
+ElementTheme SampleConfig::CurrentTheme{ ElementTheme::Default };
---
+++ Templates/EmptyTemplate/EmptyWinUICppTemplate/EmptyWinUICppTemplate/Scenario1_ShortName.xaml.h
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "Scenario1_ShortName.g.h"
+
+namespace winrt::EmptyWinUICppTemplate::implementation
+{
+    struct Scenario1_ShortName : Scenario1_ShortNameT<Scenario1_ShortName>
+    {
+        Scenario1_ShortName();
+        void SuccessMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void ErrorMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void InformationalMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void ClearMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+    private:
+        static EmptyWinUICppTemplate::MainPage rootPage;
+    };
+}
+
+namespace winrt::EmptyWinUICppTemplate::factory_implementation
+{
+    struct Scenario1_ShortName : Scenario1_ShortNameT<Scenario1_ShortName, implementation::Scenario1_ShortName>
+    {
+    };
+}
