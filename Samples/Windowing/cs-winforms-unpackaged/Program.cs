7ca55ee6ffe8114c821c19f80c3df422f7c5ee06
Samantha Song <samanthasong@microsoft.com>
2021-11-05 14:28:31 -0700

    Moving winforms samples to preview 2

--- Samples/Windowing/cs-winforms-unpackaged/Program.cs
+++ Samples/Windowing/cs-winforms-unpackaged/Program.cs
@@ -20,7 +20,7 @@ namespace winforms_unpackaged_app
             
             // Initialize dynamic dependencies so we can consume the Windows App SDK APIs from this unpackaged app.
             // Take a dependency on Windows App SDK v1.0.0-preview1.
-            int hr = MddBootstrap.Initialize(0x00010000, "preview3");
+            int hr = MddBootstrap.Initialize(0x00010000, "preview2");
             if (hr < 0)
             {
                 MessageBox.Show("Fail to initialize Dynamic Dependencies with error 0x" + hr.ToString("X"), "Error!");
