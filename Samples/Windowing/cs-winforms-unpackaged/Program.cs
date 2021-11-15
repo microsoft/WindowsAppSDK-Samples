456568494141831a8dc6cbf50299148bba911fe5
Samantha Song <samanthasong@microsoft.com>
2021-11-15 13:01:56 -0800

    Deleting mddbootstrap.cs from winforms sample and migrating to preview3

--- Samples/Windowing/cs-winforms-unpackaged/Program.cs
+++ Samples/Windowing/cs-winforms-unpackaged/Program.cs
@@ -17,19 +17,8 @@ namespace winforms_unpackaged_app
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            
-            // Initialize dynamic dependencies so we can consume the Windows App SDK APIs from this unpackaged app.
-            // Take a dependency on Windows App SDK v1.0.0-preview1.
-            int hr = MddBootstrap.Initialize(0x00010000, "preview2");
-            if (hr < 0)
-            {
-                MessageBox.Show("Fail to initialize Dynamic Dependencies with error 0x" + hr.ToString("X"), "Error!");
-                return;
-            }
-
             Application.Run(new AppForm());
 
-            MddBootstrap.Shutdown();
         }
     }
 }
