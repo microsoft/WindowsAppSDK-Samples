0f184c03039d434e03990a5474ad09c74ef0f408
Steve White <31261191+stevewhims@users.noreply.github.com>
2021-12-10 19:44:55 -0800

    Photo Editor sample; C++/WinRT version initially

---
+++ Samples/PhotoEditor/cpp-winui-packaged/PhotoEditor/App.xaml.h
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::PhotoEditor::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+        Microsoft::UI::Xaml::Controls::Frame CreateRootFrame();
+
+        void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
+        void OnNavigationFailed(IInspectable const&, Microsoft::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+
+        static winrt::Microsoft::UI::Xaml::Window Window() { return window; };
+
+    private:
+        static winrt::Microsoft::UI::Xaml::Window window;
+    };
+}
---
+++ Samples/PhotoEditor/cpp-winui-packaged/PhotoEditor/MainWindow.xaml.h
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::PhotoEditor::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+    };
+}
+
+namespace winrt::PhotoEditor::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
