a7d4c84a3b3712e81b45737b2a28168f5f99b517
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-01-10 08:14:39 -0800

    Added a C# packaging project sample.

---
+++ Samples/SelfContainedDeployment/cs-winui-packaged-wap/SelfContainedDeployment/Scenario1_SelfContainedDeployment.xaml.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Windows.ApplicationModel.Resources;
+
+namespace SelfContainedDeployment
+{
+    public partial class Scenario1_SelfContainedDeployment : Page
+    {
+        private MainPage rootPage = MainPage.Current;
+
+        public Scenario1_SelfContainedDeployment()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SuccessMessage_Click(object sender, RoutedEventArgs e)
+        {
+            rootPage.NotifyUser("Everything was ok!", InfoBarSeverity.Success);
+        }
+
+        private void ErrorMessage_Click(object sender, RoutedEventArgs e)
+        {
+            rootPage.NotifyUser("Something went wrong.", InfoBarSeverity.Error);
+        }
+
+        private void InformationalMessage_Click(object sender, RoutedEventArgs e)
+        {
+            rootPage.NotifyUser("This is the informational bar.", InfoBarSeverity.Informational);
+        }
+
+        private void ClearMessage_Click(object sender, RoutedEventArgs e)
+        {
+            rootPage.NotifyUser("", InfoBarSeverity.Informational, false);
+        }
+    }
+}
