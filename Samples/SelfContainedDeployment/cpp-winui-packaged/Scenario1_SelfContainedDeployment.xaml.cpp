7d395c4329ee18aab92aceb5b388db5902a814ae
Mike Battista <13860912+mikebattista@users.noreply.github.com>
2022-01-10 08:53:30 -0800

    Added cpp-winui-packaged.

---
+++ Samples/SelfContainedDeployment/cpp-winui-packaged/Scenario1_SelfContainedDeployment.xaml.cpp
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include "Scenario1_SelfContainedDeployment.xaml.h"
+#if __has_include("Scenario1_SelfContainedDeployment.g.cpp")
+#include "Scenario1_SelfContainedDeployment.g.cpp"
+#endif
+
+namespace winrt
+{
+    using namespace Microsoft::UI::Xaml;
+    using namespace Microsoft::UI::Xaml::Controls;
+    using namespace Windows::Foundation;
+}
+
+namespace winrt::SelfContainedDeployment::implementation
+{
+    MainPage Scenario1_SelfContainedDeployment::rootPage{ nullptr };
+
+    Scenario1_SelfContainedDeployment::Scenario1_SelfContainedDeployment()
+    {
+        InitializeComponent();
+        Scenario1_SelfContainedDeployment::rootPage = MainPage::Current();
+    }
+    
+    void Scenario1_SelfContainedDeployment::SuccessMessage_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        rootPage.NotifyUser(L"Everything was ok!", InfoBarSeverity::Success);
+    }
+
+    void Scenario1_SelfContainedDeployment::ErrorMessage_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        rootPage.NotifyUser(L"Something went wrong.", InfoBarSeverity::Error);
+    }
+
+    void Scenario1_SelfContainedDeployment::InformationalMessage_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        rootPage.NotifyUser(L"This is the informational bar.", InfoBarSeverity::Informational);
+    }
+
+    void Scenario1_SelfContainedDeployment::ClearMessage_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        rootPage.NotifyUser(L"", InfoBarSeverity::Informational);
+    }
+}
---
+++ Samples/SelfContainedDeployment/cpp-winui-packaged/Scenario1_SelfContainedDeployment.xaml.h
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "Scenario1_SelfContainedDeployment.g.h"
+
+namespace winrt::SelfContainedDeployment::implementation
+{
+    struct Scenario1_SelfContainedDeployment : Scenario1_SelfContainedDeploymentT<Scenario1_SelfContainedDeployment>
+    {
+        Scenario1_SelfContainedDeployment();
+        void SuccessMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void ErrorMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void InformationalMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void ClearMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+    private:
+        static SelfContainedDeployment::MainPage rootPage;
+    };
+}
+
+namespace winrt::SelfContainedDeployment::factory_implementation
+{
+    struct Scenario1_SelfContainedDeployment : Scenario1_SelfContainedDeploymentT<Scenario1_SelfContainedDeployment, implementation::Scenario1_SelfContainedDeployment>
+    {
+    };
+}
