12be506e9866036ea1662fe8c966794077a0452c
Bhavya Gursahani <bgursahani@microsoft.com>
2022-01-11 13:06:49 -0800

    Add CS-WINUI

---
+++ Samples/PhotoEditor/cs-winui/Photo Editor/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Core;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace PhotoEditor
+{
+    // App.xaml.cs
+    public partial class App : Application
+    {
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            Window = new MainWindow();
+            Window.Activate();
+            WindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(Window);
+        }
+        public static MainWindow Window { get; private set; }
+        public static IntPtr WindowHandle { get; private set; }
+    }
+}
+
+
+
