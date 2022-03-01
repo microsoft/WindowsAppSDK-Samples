72b935cf870d7044e5a2a7c6cbea9931ad7b27aa
Eric Langlois <erlangl@microsoft.com>
2022-03-01 09:46:48 -0800

    Initial empty sample

---
+++ Samples/Notifications/Toast/cpp-winui-unpackaged/cpp-winui-unpackaged/SampleConfiguration.h
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+#include "pch.h"
+
+namespace winrt::cpp_winui_unpackaged
+{
+    struct SampleConfig
+    {
+    public:
+        static hstring FeatureName;
+        static Microsoft::UI::Xaml::ElementTheme CurrentTheme;
+    };
+}
---
+++ Samples/Notifications/Toast/cpp-winui-unpackaged/cpp-winui-unpackaged/Scenario1_ShortName.xaml.h
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "Scenario1_ShortName.g.h"
+
+namespace winrt::cpp_winui_unpackaged::implementation
+{
+    struct Scenario1_ShortName : Scenario1_ShortNameT<Scenario1_ShortName>
+    {
+        Scenario1_ShortName();
+        void SuccessMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void ErrorMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void InformationalMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void ClearMessage_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+    private:
+        static cpp_winui_unpackaged::MainPage rootPage;
+    };
+}
+
+namespace winrt::cpp_winui_unpackaged::factory_implementation
+{
+    struct Scenario1_ShortName : Scenario1_ShortNameT<Scenario1_ShortName, implementation::Scenario1_ShortName>
+    {
+    };
+}
