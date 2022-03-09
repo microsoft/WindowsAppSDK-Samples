e741c9ede9ea7ebb067b98dd997f251b7d0553f0
Eric Langlois <erlangl@microsoft.com>
2022-03-09 12:39:45 -0800

    Now can post toasts

--- Samples/TestCppWinUiUnpackaged/TestCppWinUiUnpackaged/App.xaml.cpp
+++ Samples/TestCppWinUiUnpackaged/TestCppWinUiUnpackaged/App.xaml.cpp
@@ -2,10 +2,19 @@
 // Licensed under the MIT License.
 
 #include "pch.h"
+#include <wil/result.h>
+#include <wil/cppwinrt.h>
 #include <winrt/Windows.ApplicationModel.Activation.h>
 #include <winrt/Microsoft.Windows.AppLifecycle.h>
 #include "App.xaml.h"
 #include "MainWindow.xaml.h"
+#include <winrt/Microsoft.Windows.AppNotifications.h>
+#include <winrt/Microsoft.Windows.PushNotifications.h>
+#include <MddBootstrap.h>
+
+#include <propkey.h> //PKEY properties
+#include <propsys.h>
+#include <ShObjIdl_core.h>
 
 namespace winrt
 {
@@ -14,6 +23,33 @@ namespace winrt
     using namespace winrt::Windows::ApplicationModel::Activation;
     using namespace winrt::Microsoft::Windows::AppLifecycle;
 }
+
+// This function is intended to be called in the unpackaged scenario.
+void SetDisplayNameAndIcon() noexcept try
+{
+    // Not mandatory, but it's highly recommended to specify AppUserModelId
+    THROW_IF_FAILED(SetCurrentProcessExplicitAppUserModelID(L"TestPushAppId"));
+
+    // Icon is mandatory
+    winrt::com_ptr<IPropertyStore> propertyStore;
+    wil::unique_hwnd hWindow{ GetConsoleWindow() };
+
+    THROW_IF_FAILED(SHGetPropertyStoreForWindow(hWindow.get(), IID_PPV_ARGS(&propertyStore)));
+
+    wil::unique_prop_variant propVariantIcon;
+    wil::unique_cotaskmem_string sth = wil::make_unique_string<wil::unique_cotaskmem_string>(LR"(%SystemRoot%\system32\@WLOGO_96x96.png)");
+    propVariantIcon.pwszVal = sth.release();
+    propVariantIcon.vt = VT_LPWSTR;
+    THROW_IF_FAILED(propertyStore->SetValue(PKEY_AppUserModel_RelaunchIconResource, propVariantIcon));
+
+    // App name is not mandatory, but it's highly recommended to specify it
+    wil::unique_prop_variant propVariantAppName;
+    wil::unique_cotaskmem_string prodName = wil::make_unique_string<wil::unique_cotaskmem_string>(L"The Toast Notification Sample");
+    propVariantAppName.pwszVal = prodName.release();
+    propVariantAppName.vt = VT_LPWSTR;
+    THROW_IF_FAILED(propertyStore->SetValue(PKEY_AppUserModel_RelaunchDisplayNameResource, propVariantAppName));
+}
+CATCH_LOG()
 
 namespace winrt::TestCppWinUiUnpackaged::implementation
 {
@@ -25,6 +61,10 @@ namespace winrt::TestCppWinUiUnpackaged::implementation
         );
 
         InitializeComponent();
+
+        SetDisplayNameAndIcon(); // elx - As long as this is set, then we can post toasts
+        //winrt::Microsoft::Windows::AppNotifications::AppNotificationManager::Default().Register(); // elx - this call fails :(
+        //winrt::Microsoft::Windows::PushNotifications::PushNotificationManager::Default().Register(); // elx - this call succeeds but was only used for validation as it's not likely to be needed for this sample at this time.
 
 #if defined _DEBUG && !defined DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
         UnhandledException([](winrt::IInspectable const&, winrt::UnhandledExceptionEventArgs const& e)
