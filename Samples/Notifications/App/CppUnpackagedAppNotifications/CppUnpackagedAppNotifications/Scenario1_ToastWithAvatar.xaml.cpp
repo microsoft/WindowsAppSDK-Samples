b087377507c0dac433d48bba529ecf0b87060169
Sharath Manchala <10109130+sharath2727@users.noreply.github.com>
2022-03-11 10:38:38 -0800

    Receive cloud toasts

--- Samples/Notifications/App/CppUnpackagedAppNotifications/CppUnpackagedAppNotifications/Scenario1_ToastWithAvatar.xaml.cpp
+++ Samples/Notifications/App/CppUnpackagedAppNotifications/CppUnpackagedAppNotifications/Scenario1_ToastWithAvatar.xaml.cpp
@@ -2,18 +2,25 @@
 // Licensed under the MIT License.
 
 #include "pch.h"
-#include <winrt/Microsoft.Windows.AppNotifications.h>
 #include "Scenario1_ToastWithAvatar.xaml.h"
 #if __has_include("Scenario1_ToastWithAvatar.g.cpp")
 #include "Scenario1_ToastWithAvatar.g.cpp"
+#include <winrt/Microsoft.Windows.AppNotifications.h>
+#include <winrt/Microsoft.Windows.PushNotifications.h>
+#include <shobjidl_core.h>
+#include <wil/result.h>
+#include <iostream>
+#include <wil/win32_helpers.h>
 #endif
 
 namespace winrt
 {
-    using namespace Microsoft::UI::Xaml;
-    using namespace Microsoft::UI::Xaml::Controls;
-    using namespace Windows::Foundation;
-    using namespace Microsoft::Windows::AppNotifications;
+    using namespace winrt::Microsoft::UI::Xaml;
+    using namespace winrt::Microsoft::UI::Xaml::Controls;
+    using namespace winrt::Windows::Foundation;
+    using namespace winrt::Microsoft::Windows::AppNotifications;
+    using namespace winrt::Microsoft::Windows::PushNotifications;
+    using namespace winrt::Windows::Foundation::Collections;
 }
 
 namespace winrt::CppUnpackagedAppNotifications::implementation
@@ -24,8 +31,87 @@ namespace winrt::CppUnpackagedAppNotifications::implementation
     {
         InitializeComponent();
         Scenario1_ToastWithAvatar::rootPage = MainPage::Current();
+
+        m_appNotificationManager =  winrt::AppNotificationManager::Default();
     }
-    
+
+    winrt::IAsyncOperation<winrt::PushNotificationChannel> Scenario1_ToastWithAvatar::RequestChannelAsync()
+    {
+        // To obtain an AAD RemoteIdentifier for your app,
+        // follow the instructions on https://docs.microsoft.com/azure/active-directory/develop/quickstart-register-app
+        auto channelOperation = winrt::PushNotificationManager::Default().CreateChannelAsync(
+            winrt::guid("0160ee84-0c53-4851-9ff2-d7f5a87ed914")).get();
+
+      //  auto result = co_await channelOperation;
+
+        if (channelOperation.Status() == winrt::PushNotificationChannelStatus::CompletedSuccess)
+        {
+            auto channelUri{ channelOperation.Channel().Uri() };
+
+            std::cout << "channelUri: " << winrt::to_string(channelUri.ToString()) << std::endl << std::endl;
+            
+            auto channelExpiry = channelOperation.Channel().ExpirationTime();
+
+            // Caller's responsibility to keep the channel alive
+            co_return channelOperation.Channel();
+        }
+        else if (channelOperation.Status() == winrt::PushNotificationChannelStatus::CompletedFailure)
+        {
+           // LOG_HR_MSG(result.ExtendedError(), "We hit a critical non-retryable error with channel request!");
+            std::cout << "We hit a critical non-retryable error with channel request!";
+            co_return nullptr;
+        }
+        else
+        {
+           // LOG_HR_MSG(result.ExtendedError(), "Some other failure occurred.");
+            std::cout << "Some other failure occurred.";
+            co_return nullptr;
+        }
+
+    };
+
+    winrt::PushNotificationChannel Scenario1_ToastWithAvatar::RequestChannel()
+    {
+        auto task = RequestChannelAsync();
+        if (task.wait_for(std::chrono::seconds(300)) != winrt::AsyncStatus::Completed)
+        {
+            task.Cancel();
+            return nullptr;
+        }
+
+        auto result = task.GetResults();
+        return result;
+    }
+
+    void Scenario1_ToastWithAvatar::RequestChannel_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+       m_channel = RequestChannel();
+
+        rootPage.NotifyUser(winrt::to_hstring(m_channel.Uri().ToString()).c_str(), InfoBarSeverity::Success);
+    }
+
+    void Scenario1_ToastWithAvatar::RegisterManager_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        THROW_IF_FAILED(SetCurrentProcessExplicitAppUserModelID(L"TestPushAppId3")); // elx - need to get a proper app id
+
+        winrt::event_token token = m_appNotificationManager.NotificationInvoked([](const auto&, winrt::AppNotificationActivatedEventArgs const& toastArgs)
+            {
+                std::wcout << L"AppNotification received foreground!\n";
+                winrt::hstring arguments{ toastArgs.Argument() };
+                std::wcout << arguments.c_str() << L"\n\n";
+
+                winrt::IMap<winrt::hstring, winrt::hstring> userInput{ toastArgs.UserInput() };
+                for (auto pair : userInput)
+                {
+                    std::wcout << "Key= " << pair.Key().c_str() << " " << "Value= " << pair.Value().c_str() << L"\n";
+                }
+                std::wcout << L"\n";
+            });
+
+        m_appNotificationManager.Register();
+
+        rootPage.NotifyUser(L"AppNotificationManager Registered COM Activator successful!", InfoBarSeverity::Success);
+    }
     void Scenario1_ToastWithAvatar::SendToast_Click(IInspectable const&, RoutedEventArgs const&)
     {
         winrt::hstring xmlPayload{ L"<toast>message</toast>" };
--- Samples/Notifications/App/CppUnpackagedAppNotifications/CppUnpackagedAppNotifications/Scenario1_ToastWithAvatar.xaml.h
+++ Samples/Notifications/App/CppUnpackagedAppNotifications/CppUnpackagedAppNotifications/Scenario1_ToastWithAvatar.xaml.h
@@ -4,15 +4,27 @@
 #pragma once
 
 #include "Scenario1_ToastWithAvatar.g.h"
+#include <winrt/Microsoft.Windows.AppNotifications.h>
+#include <winrt/Microsoft.Windows.PushNotifications.h>
+#include <winrt/Windows.Foundation.h>
 
 namespace winrt::CppUnpackagedAppNotifications::implementation
 {
     struct Scenario1_ToastWithAvatar : Scenario1_ToastWithAvatarT<Scenario1_ToastWithAvatar>
     {
         Scenario1_ToastWithAvatar();
+
+        void RequestChannel_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+        void RegisterManager_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
         void SendToast_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Microsoft::UI::Xaml::RoutedEventArgs const&);
+
     private:
         static CppUnpackagedAppNotifications::MainPage rootPage;
+        winrt::Microsoft::Windows::AppNotifications::AppNotificationManager m_appNotificationManager {nullptr};
+        winrt::Microsoft::Windows::PushNotifications::PushNotificationChannel m_channel {nullptr};
+
+        winrt::Microsoft::Windows::PushNotifications::PushNotificationChannel RequestChannel();
+        winrt::Windows::Foundation::IAsyncOperation<winrt::Microsoft::Windows::PushNotifications::PushNotificationChannel> RequestChannelAsync();
     };
 }
 
