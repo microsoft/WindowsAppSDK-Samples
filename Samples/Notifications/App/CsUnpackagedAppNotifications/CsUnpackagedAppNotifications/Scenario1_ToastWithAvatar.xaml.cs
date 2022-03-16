539316571b835232a2cff29456cf4ad55735cfa2
Eric Langlois <erlangl@microsoft.com>
2022-03-16 07:26:06 -0700

    code cleanup

--- Samples/Notifications/App/CsUnpackagedAppNotifications/CsUnpackagedAppNotifications/Scenario1_ToastWithAvatar.xaml.cs
+++ Samples/Notifications/App/CsUnpackagedAppNotifications/CsUnpackagedAppNotifications/Scenario1_ToastWithAvatar.xaml.cs
@@ -19,28 +19,28 @@ namespace CsUnpackagedAppNotifications
         {
 #if false
             winrt::hstring xmlPayload{
-            L"<toast>\
-                <visual>\
-                    <binding template=\"ToastGeneric\">\
-                        <text>App Notifications Sample Scenario 1</text>\
-                        <text>This is an example message using XML</text>\
-                    </binding>\
-                </visual>\
-                <actions>\
-                    <action\
-                        content = \"Activate Toast\"\
-                        arguments = \"action=viewDetails&amp;contentId=351\"\
-                        activationType = \"foreground\" />\
-                </actions>\
-            </toast>" };
+                "<toast>\
+                    <visual>\
+                        <binding template=\"ToastGeneric\">\
+                            <text>App Notifications Sample Scenario 1</text>\
+                            <text>This is an example message using XML</text>\
+                        </binding>\
+                    </visual>\
+                    <actions>\
+                        <action\
+                            content = \"Activate Toast\"\
+                            arguments = \"action=viewDetails&amp;contentId=351\"\
+                            activationType = \"foreground\" />\
+                    </actions>\
+                </toast>" };
 
-        auto toast{ winrt::AppNotification(xmlPayload) };
-        toast.Priority(winrt::AppNotificationPriority::High);
-        winrt::AppNotificationManager::Default().Show(toast);
-        if (toast.Id() == 0)
-        {
-            rootPage.NotifyUser("Could not send toast", InfoBarSeverity.Error);
-        }
+            auto toast{ winrt::AppNotification(xmlPayload) };
+            toast.Priority(winrt::AppNotificationPriority::High);
+            winrt::AppNotificationManager::Default().Show(toast);
+            if (toast.Id() == 0)
+            {
+                rootPage.NotifyUser("Could not send toast", InfoBarSeverity.Error);
+            }
 #endif
             rootPage.NotifyUser("Toast sent successfully!", InfoBarSeverity.Success);
         }
