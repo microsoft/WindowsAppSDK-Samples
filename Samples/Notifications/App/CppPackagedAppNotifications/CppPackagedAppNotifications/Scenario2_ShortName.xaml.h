a198d43fdab53884c6e6cfae6176ffded263024b
Eric Langlois <erlangl@microsoft.com>
2022-03-23 13:24:10 -0700

    Adding a packaged project for testing only

---
+++ Samples/Notifications/App/CppPackagedAppNotifications/CppPackagedAppNotifications/Scenario2_ShortName.xaml.h
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "Scenario2_ShortName.g.h"
+
+namespace winrt::CppPackagedAppNotifications::implementation
+{
+    struct Scenario2_ShortName : Scenario2_ShortNameT<Scenario2_ShortName>
+    {
+        Scenario2_ShortName();
+    };
+}
+
+namespace winrt::CppPackagedAppNotifications::factory_implementation
+{
+    struct Scenario2_ShortName : Scenario2_ShortNameT<Scenario2_ShortName, implementation::Scenario2_ShortName>
+    {
+    };
+}
