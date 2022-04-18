841a3f376c5207448f9774b975e69aab59094ae6
Will Thant <kythant@microsoft.com>
2022-04-18 16:46:24 -0700

    fix quotes

--- UpdateVersions.ps1
+++ UpdateVersions.ps1
@@ -21,16 +21,16 @@ Get-ChildItem -Recurse *.vcxproj -Path $PSScriptRoot | foreach-object {
 }
 
 Get-ChildItem -Recurse *.wapproj -Path $PSScriptRoot | foreach-object {
-    $newVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version='+ $WinAppSDKVersion + '"'
-    $oldVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version='+ $WinAppSDKVersionOld + '"'
+    $newVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version="'+ $WinAppSDKVersion + '"'
+    $oldVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version="'+ $WinAppSDKVersionOld + '"'
     $content = Get-Content $_.FullName -Raw
     $content = $content.replace($oldVersionString, $newVersionString)
     Set-Content -Path $_.FullName -Value $content
 }
 
 Get-ChildItem -Recurse *.csproj -Path $PSScriptRoot | foreach-object {
-    $newVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version='+ $WinAppSDKVersion + '"'
-    $oldVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version='+ $WinAppSDKVersionOld + '"'
+    $newVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version="'+ $WinAppSDKVersion + '"'
+    $oldVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version="'+ $WinAppSDKVersionOld + '"'
     $content = Get-Content $_.FullName -Raw
     $content = $content.replace($oldVersionString, $newVersionString)
     Set-Content -Path $_.FullName -Value $content
