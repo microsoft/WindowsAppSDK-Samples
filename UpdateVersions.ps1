bd1d8ffe4c68243c6e5f61055a79527ef54edfe6
Will Thant <kythant@microsoft.com>
2022-04-18 16:41:10 -0700

    Vcxprojs

--- UpdateVersions.ps1
+++ UpdateVersions.ps1
@@ -4,12 +4,18 @@ Param(
     [string]$RootPath = ""
 )
 
-Write-Host "Updating"
 Get-ChildItem -Recurse packages.config -Path $PSScriptRoot | foreach-object {
     $newVersionString = 'version="' + $WinAppSDKVersion + '"'
     $oldVersionString = 'version="' + $WinAppSDKVersionOld + '"'
     $content = Get-Content $_.FullName -Raw
-    Write-Host $content
+    $content = $content.replace($oldVersionString, $newVersionString)
+    Set-Content -Path $_.FullName -Value $content
+  }
+
+Get-ChildItem -Recurse *.vcxproj -Path $PSScriptRoot | foreach-object {
+    $newVersionString = 'Microsoft.WindowsAppSDK.' + $WinAppSDKVersion
+    $oldVersionString = 'Microsoft.WindowsAppSDK.' + $WinAppSDKVersionOld
+    $content = Get-Content $_.FullName -Raw
     $content = $content.replace($oldVersionString, $newVersionString)
     Set-Content -Path $_.FullName -Value $content
   }
