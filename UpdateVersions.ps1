d2e8f9ea8944c8c1c2028010acfd83910e28ea79
Will Thant <kythant@microsoft.com>
2022-04-18 16:25:59 -0700

    Fix param

--- UpdateVersions.ps1
+++ UpdateVersions.ps1
@@ -1,6 +1,6 @@
 Param(
     [string]$WinAppSDKVersion = "",
-    [switch]$WinAppSDKVersionOld = "1.0.0"
+    [string]$WinAppSDKVersionOld = "1.0.0"
 )
 
 Get-ChildItem packages.config -Path $PSScriptRoot | foreach-object {
