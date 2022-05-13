53322c744ae808d589667b7d0ce2b050845405cd
btueffers <82911735+btueffers@users.noreply.github.com>
2022-05-12 23:20:15 -0700

    cs winforms unpackaged

--- Samples/AppLifecycle/EnvironmentVariables/cs-winforms-unpackaged/CsWinFormsEnv/Program.cs
+++ Samples/AppLifecycle/EnvironmentVariables/cs-winforms-unpackaged/CsWinFormsEnv/Program.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation.
+﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
 using System;
@@ -8,9 +8,6 @@ namespace CsWinFormsEnv
 {
     static class Program
     {
-        // Windows App SDK version.
-        static uint majorMinorVersion = 0x00010000;
-        static string versionTag = "";
 
         [STAThread]
         static void Main()
@@ -19,15 +16,8 @@ namespace CsWinFormsEnv
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Initialize Windows App SDK for unpackaged apps.            
-            int result = MddBootstrap.Initialize(majorMinorVersion, versionTag);
-            if (result == 0)
-            {
-                Application.Run(new MainForm());
+            Application.Run(new MainForm());
 
-                // Uninitialize Windows App SDK.
-                MddBootstrap.Shutdown();
-            }
         }
     }
 }
