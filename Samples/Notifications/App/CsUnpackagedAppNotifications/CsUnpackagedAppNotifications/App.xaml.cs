bbbdcf30ab44c25823a856bec531a04df21a2a7b
eric langlois <email@ericlanglois.com>
2022-05-23 10:35:18 -0700

    User/erlangl/samples/toast c# winui unpackaged (#203)
    * Initial empty sample
    
    * matching naming of the cpp sample
    
    * replaced with new app notifications sample
    
    * There's only one scenario for now
    
    * Implementing scenario 1
    
    * code cleanup
    
    * Adding second scenario
    
    * Update to preview3 SDK
    
    * Can show a toast
    
    * Scenario 2 can show toast
    
    * alignment
    
    * Seup a notification handler
    
    * wip
    
    * Route received notifications to the correct scenario
    
    * Scenario formats received notifications and pass to UI
    
    * Adding support for toast notification activation
    
    * Scenario descriptions were flipped
    
    * Temp workaround listview issue
    
    * Fixing path to icon
    
    * Fixing issue with updating received activations in the UI
    
    * Ensuring Unregister is called on app exit
    
    * Code clean-up
    
    * Update to match C++ sample
    
    * Code clean-up
    
    * code clean-up
    
    * nits
    
    * Bringing the sample to par with the C++ one
    
    * Adding launch arguments to toasts
    
    * Adding comment
    
    * Bringing this sample to par with its cpp counterpart
    
    * Simplify interop call
    
    Co-authored-by: Eric Langlois <erlangl@microsoft.com>

---
+++ Samples/Notifications/App/CsUnpackagedAppNotifications/CsUnpackagedAppNotifications/App.xaml.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Windows.AppLifecycle;
+using Microsoft.UI.Xaml;
+using Microsoft.Windows.AppNotifications;
+using Windows.Win32.Foundation;
+using System;
+using CsUnpackagedAppNotifications.Notifications;
+using static Windows.Win32.PInvoke;
+
+namespace CsUnpackagedAppNotifications
+{
+
+    public partial class App : Application
+    {
+        private static Window mainWindow = null;
+        private NotificationManager notificationManager;
+
+        public App()
+        {
+            this.InitializeComponent();
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+
+            notificationManager = new NotificationManager();
+        }
+
+    	public static void ToForeground()
+    	{
+            if (mainWindow != null)
+            {
+                HWND hwnd = (HWND)WinRT.Interop.WindowNative.GetWindowHandle(mainWindow);
+                SwitchToThisWindow(hwnd, true);
+            }
+        }
+	
+        public static string GetFullPathToExe()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var pos = path.LastIndexOf("\\");
+            return path.Substring(0, pos);
+        }
+
+        public static string GetFullPathToAsset(string assetName)
+        {
+            return GetFullPathToExe() + "\\Assets\\" + assetName;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            mainWindow = new MainWindow();
+
+            notificationManager.Init();
+
+            // NOTE: AppInstance is ambiguous between
+            // Microsoft.Windows.AppLifecycle.AppInstance and
+            // Windows.ApplicationModel.AppInstance
+            var currentInstance = AppInstance.GetCurrent();
+            if (currentInstance.IsCurrent)
+            {
+                // AppInstance.GetActivatedEventArgs will report the correct ActivationKind,
+                // even in WinUI's OnLaunched.
+                AppActivationArguments activationArgs = currentInstance.GetActivatedEventArgs();
+                if (activationArgs != null)
+                {
+                    ExtendedActivationKind extendedKind = activationArgs.Kind;
+                    if (extendedKind == ExtendedActivationKind.AppNotification)
+                    {
+                        var notificationActivatedEventArgs = (AppNotificationActivatedEventArgs)activationArgs.Data;
+                        notificationManager.ProcessLaunchActivationArgs(notificationActivatedEventArgs);
+                    }
+                }
+            }
+
+            mainWindow.Activate();
+        }
+
+        void OnProcessExit(object sender, EventArgs e)
+        {
+            notificationManager.Unregister();
+        }
+    }
+}
---
+++ Samples/Notifications/App/CsUnpackagedAppNotifications/CsUnpackagedAppNotifications/Notifications/ToastWithAvatar.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using CsUnpackagedAppNotifications;
+using Microsoft.Windows.AppNotifications;
+
+class ToastWithAvatar
+{
+    public const int ScenarioId = 1;
+    public const string ScenarioName = "Local Toast with Avatar Image";
+
+    public static bool SendToast()
+    {
+        // The ScenarioIdToken uniquely identify a scenario and is used to route the response received when the user clicks on a toast to the correct scenario.
+        var ScenarioIdToken = Common.MakeScenarioIdToken(ScenarioId);
+	
+        var xmlPayload = new string(
+        	"<toast launch = \"action=ToastClick&amp;" + ScenarioIdToken + "\">"
+        +       "<visual>"
+        +           "<binding template = \"ToastGeneric\">"
+        +               "<image placement = \"appLogoOverride\" hint-crop=\"circle\" src = \"" + App.GetFullPathToAsset("Square150x150Logo.png") + "\"/>"
+        +               "<text>" + ScenarioName + "</text>"
+        +               "<text>This is an example message using XML</text>"
+        +           "</binding>"
+        +       "</visual>"
+        +       "<actions>"
+        +           "<action "
+        +               "content = \"Open App\" "
+        +               "arguments = \"action=OpenApp&amp;" + ScenarioIdToken + "\"/>"
+        +       "</actions>"
+        +   "</toast>" );
+
+        var toast = new AppNotification(xmlPayload);
+        AppNotificationManager.Default.Show(toast);
+
+    	return toast.Id != 0; // return true (indicating success) if the toast was sent (if it has an Id)
+
+    }
+
+    public static void NotificationReceived(AppNotificationActivatedEventArgs notificationActivatedEventArgs)
+    {
+        var notification = new MainPage.Notification();
+        notification.Originator = ScenarioName;
+        var action = Common.ExtractParamFromArgs(notificationActivatedEventArgs.Argument, "action");
+        notification.Action = action == null ? "" : action;
+        MainPage.Current.NotificationReceived(notification);
+        App.ToForeground();
+    }
+}
