bd7b7f59344cb17dd0ca404dbc3415abc92e3db3
btueffers <82911735+btueffers@users.noreply.github.com>
2022-05-24 11:00:16 -0700

    Environment Variables (#206)
    * Initial check-in
    
    * Use WindowsPackageType property
    
    * cpp win32 unpackaged
    
    * cs winforms unpackaged
    
    * Removing version variables no longer needed
    
    * adding arm configurations

---
+++ Samples/AppLifecycle/EnvironmentVariables/cs-winforms-unpackaged/CsWinFormsEnv/Program.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Windows.Forms;
+
+namespace CsWinFormsEnv
+{
+    static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new MainForm());
+
+        }
+    }
+}
