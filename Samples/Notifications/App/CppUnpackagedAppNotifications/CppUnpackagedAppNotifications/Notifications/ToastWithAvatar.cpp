8cc5221a5584fbcbee5670b66dde634169df73b3
Eric Langlois <erlangl@microsoft.com>
2022-08-09 08:02:43 -0700

    Using Builder instead of Raw XML

--- Samples/Notifications/App/CppUnpackagedAppNotifications/CppUnpackagedAppNotifications/Notifications/ToastWithAvatar.cpp
+++ Samples/Notifications/App/CppUnpackagedAppNotifications/CppUnpackagedAppNotifications/Notifications/ToastWithAvatar.cpp
@@ -5,12 +5,16 @@
 #include "ToastWithAvatar.h"
 #include "Common.h"
 #include <winrt/Microsoft.Windows.AppNotifications.h>
+#include <winrt/Microsoft.Windows.AppNotifications.Builder.h>
+#include <winrt/Windows.Foundation.h>
 #include "App.xaml.h"
 #include "MainPage.xaml.h"
 
 namespace winrt
 {
     using namespace Microsoft::Windows::AppNotifications;
+    using namespace Microsoft::Windows::AppNotifications::Builder;
+
     using namespace CppUnpackagedAppNotifications::implementation;
 }
 
@@ -19,8 +23,8 @@ const wchar_t* ToastWithAvatar::ScenarioName{ L"Local Toast with Avatar Image" }
 bool ToastWithAvatar::SendToast()
 {
     // The ScenarioIdToken uniquely identify a scenario and is used to route the response received when the user clicks on a toast to the correct scenario.
-    auto ScenarioIdToken{ Common::MakeScenarioIdToken(ToastWithAvatar::ScenarioId) };
-
+    //auto ScenarioIdToken{ Common::MakeScenarioIdToken(ToastWithAvatar::ScenarioId) };
+#if 0
     winrt::hstring xmlPayload{
         L"<toast launch = \"action=ToastClick&amp;" + ScenarioIdToken + L"\">\
             <visual>\
@@ -38,9 +42,22 @@ bool ToastWithAvatar::SendToast()
         </toast>" };
 
     auto toast{ winrt::AppNotification(xmlPayload) };
-    winrt::AppNotificationManager::Default().Show(toast);
+#endif
+    winrt::Microsoft::Windows::AppNotifications::Builder::AppNotificationBuilder();
+    auto appNotification{ winrt::AppNotification(winrt::AppNotificationBuilder()
+        .AddArgument(L"action", L"ToastClick")
+        .AddArgument(L"scenarioId", std::to_wstring(ToastWithAvatar::ScenarioId))
+        .SetAppLogoOverride(winrt::Windows::Foundation::Uri(L"file://" + winrt::App::GetFullPathToAsset(L"Square150x150Logo.png")), winrt::AppNotificationImageCrop::Circle)
+        .AddText(ScenarioName)
+        .AddText(L"This is an example message using XML")
+        .AddButton(winrt::AppNotificationButton(L"Open App")
+            .AddArgument(L"action", L"OpenApp")
+            .AddArgument(L"scenarioId", std::to_wstring(ToastWithAvatar::ScenarioId)))
+        .GetXmlPayload()) };
 
-    return toast.Id() != 0; // return true (indicating success) if the toast was sent (if it has an Id)
+    winrt::AppNotificationManager::Default().Show(appNotification);
+
+    return appNotification.Id() != 0; // return true (indicating success) if the toast was sent (if it has an Id)
 }
 
 void ToastWithAvatar::NotificationReceived(winrt::Microsoft::Windows::AppNotifications::AppNotificationActivatedEventArgs const& notificationActivatedEventArgs)
