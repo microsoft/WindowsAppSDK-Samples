2162f9fd864f25c15eba55dd0e62ffdc20919175
Manodasan Wignarajah <mawign@microsoft.com>
2022-09-19 11:45:26 -0700

    Fix UpdateVersions.ps1 to accomodate that samples can be on different versions.

--- UpdateVersions.ps1
+++ UpdateVersions.ps1
@@ -1,40 +1,39 @@
 Param(
-    [string]$WinAppSDKVersion = "",
-    [string]$WinAppSDKVersionOld = "1.0.0"
+    [string]$WinAppSDKVersion = ""
 )
 
 Get-ChildItem -Recurse packages.config -Path $PSScriptRoot | foreach-object {
     $newVersionString = 'package id="Microsoft.WindowsAppSDK" version="' + $WinAppSDKVersion + '"'
-    $oldVersionString = 'package id="Microsoft.WindowsAppSDK" version="' + $WinAppSDKVersionOld + '"'
+    $oldVersionString = 'package id="Microsoft.WindowsAppSDK" version="[-.0-9a-zA-Z]*"'
     $content = Get-Content $_.FullName -Raw
-    $content = $content.replace($oldVersionString, $newVersionString)
+    $content = $content -replace $oldVersionString, $newVersionString
     Set-Content -Path $_.FullName -Value $content
     Write-Host "Modified " $_.FullName 
 }
 
 Get-ChildItem -Recurse *.vcxproj -Path $PSScriptRoot | foreach-object {
-    $newVersionString = 'packages\Microsoft.WindowsAppSDK.' + $WinAppSDKVersion
-    $oldVersionString = 'packages\Microsoft.WindowsAppSDK.' + $WinAppSDKVersionOld
+    $newVersionString = 'packages\Microsoft.WindowsAppSDK.' + $WinAppSDKVersion + '\'
+    $oldVersionString = 'packages\\Microsoft.WindowsAppSDK.[-.0-9a-zA-Z]*\\'
     $content = Get-Content $_.FullName -Raw
-    $content = $content.replace($oldVersionString, $newVersionString)
+    $content = $content -replace $oldVersionString, $newVersionString
     Set-Content -Path $_.FullName -Value $content
     Write-Host "Modified " $_.FullName 
 }
 
 Get-ChildItem -Recurse *.wapproj -Path $PSScriptRoot | foreach-object {
     $newVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version="'+ $WinAppSDKVersion + '"'
-    $oldVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version="'+ $WinAppSDKVersionOld + '"'
+    $oldVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version="[-.0-9a-zA-Z]*"'
     $content = Get-Content $_.FullName -Raw
-    $content = $content.replace($oldVersionString, $newVersionString)
+    $content = $content -replace $oldVersionString, $newVersionString
     Set-Content -Path $_.FullName -Value $content
     Write-Host "Modified " $_.FullName 
 }
 
 Get-ChildItem -Recurse *.csproj -Path $PSScriptRoot | foreach-object {
     $newVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version="'+ $WinAppSDKVersion + '"'
-    $oldVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version="'+ $WinAppSDKVersionOld + '"'
+    $oldVersionString = 'PackageReference Include="Microsoft.WindowsAppSDK" Version="[-.0-9a-zA-Z]*"'
     $content = Get-Content $_.FullName -Raw
-    $content = $content.replace($oldVersionString, $newVersionString)
+    $content = $content -replace $oldVersionString, $newVersionString
     Set-Content -Path $_.FullName -Value $content
     Write-Host "Modified " $_.FullName 
 }
