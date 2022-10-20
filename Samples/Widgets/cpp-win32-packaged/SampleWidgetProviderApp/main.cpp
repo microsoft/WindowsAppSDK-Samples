1534e82577000d85e709aebaa786c5e79dd72d72
Seva Iliushin <sevkorobot@gmail.com>
2022-10-20 16:38:04 -0700

    add shutdown logic; update readme

--- Samples/Widgets/cpp-win32-packaged/SampleWidgetProviderApp/main.cpp
+++ Samples/Widgets/cpp-win32-packaged/SampleWidgetProviderApp/main.cpp
@@ -5,8 +5,6 @@
 #include "WidgetProvider.h"
 #include <mutex>
 #include <winstring.h>
-#include <iostream>
-#include <objidl.h>
 
 // This GUID is the same GUID that was provided in the 
 // registration of the COM Server and Class Id in the .appxmanifest.
@@ -15,10 +13,17 @@ static constexpr GUID widget_provider_clsid
     0x101d03a3, 0x6fc8, 0x4887, {0x9b, 0x64, 0x31, 0x0a, 0x7b, 0x16, 0x43, 0x19}
 };
 
+wil::unique_event g_shudownEvent(wil::EventOptions::None);
+
+void SignalLocalServerShutdown()
+{
+    g_shudownEvent.SetEvent();
+}
+
 // This is implementation of a ClassFactory that will instantiate WidgetProvider
 // that will interact with the Widget Service.
 template <typename T>
-struct SingletonClassFactory : winrt::implements<SingletonClassFactory<T>, IClassFactory>
+struct SingletonClassFactory : winrt::implements<SingletonClassFactory<T>, IClassFactory, winrt::no_module_lock>
 {
     STDMETHODIMP CreateInstance(
         ::IUnknown* outer,
@@ -33,49 +38,23 @@ struct SingletonClassFactory : winrt::implements<SingletonClassFactory<T>, IClas
         {
             return CLASS_E_NOAGGREGATION;
         }
-
-        return winrt::make_self<T>().as(iid, result);
+        
+        return winrt::make<T>().as(iid, result);
     }
 
-    STDMETHODIMP LockServer(BOOL flock) noexcept final
+    STDMETHODIMP LockServer(BOOL) noexcept final
     {
         return S_OK;
     }
 
 private:
-    //winrt::com_ptr<T> instance{ nullptr };
     std::mutex mutex;
 };
-
-#include <windows.h>
-
-inline HMODULE GetComBaseModuleHandle()
-{
-    /*
-    CoGetSharedServiceId @66 NONAME PRIVATE_API
-    CoAddRefSharedService @67 NONAME PRIVATE_API
-    CoReleaseSharedService @68 NONAME PRIVATE_API
-    CoRegisterServerShutdownDelay @69 NONAME PRIVATE_API
-*/
-    static HMODULE s_hmod = LoadLibraryW(L"ComBase.dll");
-    return s_hmod;
-}
-
-inline HRESULT __stdcall CoRegisterServerShutdownDelay(HANDLE stopEvent, DWORD milliseconds)
-{
-    static auto s_fn = reinterpret_cast<decltype(CoRegisterServerShutdownDelay)*>(GetProcAddress(GetComBaseModuleHandle(), MAKEINTRESOURCEA(69)));
-    return s_fn ? s_fn(stopEvent, milliseconds) : E_UNEXPECTED;
-}
 
 int main()
 {
     winrt::init_apartment();
-    winrt::com_ptr<IGlobalOptions> globalOptions;
-    if (SUCCEEDED(CoCreateInstance(CLSID_GlobalOptions, nullptr, CLSCTX_INPROC, IID_PPV_ARGS(&globalOptions))))
-    {
-        globalOptions->Set(COMGLB_EXCEPTION_HANDLING, COMGLB_EXCEPTION_DONOT_HANDLE_ANY);
-        globalOptions->Set(COMGLB_RO_SETTINGS, COMGLB_FAST_RUNDOWN);
-    }
+
     wil::unique_com_class_object_cookie widgetProviderFactory;
     // Create WidgetProvider factory
     auto factory = winrt::make<SingletonClassFactory<WidgetProvider>>();
@@ -88,12 +67,10 @@ int main()
         REGCLS_MULTIPLEUSE,
         widgetProviderFactory.put()));
 
-    wil::unique_event shudownEvent(wil::EventOptions::None);
-    winrt::check_hresult(CoRegisterServerShutdownDelay(shudownEvent.get(), 1 * 1000)); // 5 seconds
-
     DWORD index{};
-    HANDLE events[] = { shudownEvent.get() };
-    winrt::check_hresult(CoWaitForMultipleObjects(CWMO_DISPATCH_CALLS | CWMO_DISPATCH_WINDOW_MESSAGES, INFINITE, std::size(events), events, &index));
+    HANDLE events[] = { g_shudownEvent.get() };
+    winrt::check_hresult(CoWaitForMultipleObjects(CWMO_DISPATCH_CALLS | CWMO_DISPATCH_WINDOW_MESSAGES, INFINITE,
+        static_cast<ULONG>(std::size(events)), events, &index));
 
     return 0;
 }
--- Samples/Widgets/cpp-win32-packaged/SampleWidgetProviderApp/pch.h
+++ Samples/Widgets/cpp-win32-packaged/SampleWidgetProviderApp/pch.h
@@ -2,6 +2,45 @@
 // Licensed under the MIT License.
 
 #pragma once
+
+#include <stdint.h>
+#include <combaseapi.h>
+
+// In .exe local servers the class object must not contribute to the module ref count, and use
+// winrt::no_module_lock, the other objects must and this is the hook into the C++ WinRT ref counting system
+// that enables this.
+
+void SignalLocalServerShutdown();
+
+namespace winrt
+{
+    inline auto get_module_lock() noexcept
+    {
+        struct service_lock
+        {
+            uint32_t operator++() noexcept
+            {
+                return ::CoAddRefServerProcess();
+            }
+
+            uint32_t operator--() noexcept
+            {
+                const auto ref = ::CoReleaseServerProcess();
+
+                if (ref == 0)
+                {
+                    SignalLocalServerShutdown();
+                }
+                return ref;
+            }
+        };
+
+        return service_lock{};
+    }
+}
+
+#define WINRT_CUSTOM_MODULE_LOCK
+
 #include <wil/cppwinrt.h> // Already includes unknwn.h for COM support
 #include <wil/resource.h>
 
