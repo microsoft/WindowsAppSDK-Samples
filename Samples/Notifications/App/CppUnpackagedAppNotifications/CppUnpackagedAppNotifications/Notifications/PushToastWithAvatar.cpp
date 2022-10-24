b390638fed834d5f7833f9bc9ed8d112c8ba16fc
Eric Langlois <erlangl@microsoft.com>
2022-10-24 13:58:05 -0700

    New scenario to showcase push / toast mixed scenarios

---
+++ Samples/Notifications/App/CppUnpackagedAppNotifications/CppUnpackagedAppNotifications/Notifications/PushToastWithAvatar.cpp
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include "PushToastWithAvatar.h"
+#include "Common.h"
+#include <winrt/Microsoft.Windows.AppNotifications.h>
+#include <winrt/Microsoft.Windows.AppNotifications.Builder.h>
+#include <winrt/Windows.Foundation.h>
+#include "App.xaml.h"
+#include "MainPage.xaml.h"
+
+namespace winrt
+{
+    using namespace Microsoft::Windows::AppNotifications;
+    using namespace Microsoft::Windows::AppNotifications::Builder;
+    using namespace CppUnpackagedAppNotifications::implementation;
+}
+
+const wchar_t* PushToastWithAvatar::ScenarioName{ L"Push / Local Toast with Avatar Image" };
+
+bool PushToastWithAvatar::SendToast()
+{
+    auto appNotification{ winrt::AppNotificationBuilder()
+        .AddArgument(L"action", L"ToastClick")
+        .AddArgument(Common::scenarioTag, std::to_wstring(PushToastWithAvatar::ScenarioId))
+        .SetAppLogoOverride(winrt::Windows::Foundation::Uri(L"file://" + winrt::App::GetFullPathToAsset(L"Square150x150Logo.png")), winrt::AppNotificationImageCrop::Circle)
+        .AddText(ScenarioName)
+        .AddText(L"This is an example message using XML")
+        .AddButton(winrt::AppNotificationButton(L"Open App")
+            .AddArgument(L"action", L"OpenApp")
+            .AddArgument(Common::scenarioTag, std::to_wstring(PushToastWithAvatar::ScenarioId)))
+        .BuildNotification() };
+
+    winrt::AppNotificationManager::Default().Show(appNotification);
+
+    return appNotification.Id() != 0; // return true (indicating success) if the toast was sent (if it has an Id)
+}
+
+void PushToastWithAvatar::NotificationReceived(winrt::Microsoft::Windows::AppNotifications::AppNotificationActivatedEventArgs const& notificationActivatedEventArgs)
+{
+    winrt::CppUnpackagedAppNotifications::Notification notification{};
+    notification.Originator = ScenarioName;
+    notification.Action = notificationActivatedEventArgs.Arguments().Lookup(L"action");
+    winrt::MainPage::Current().NotificationReceived(notification);
+    winrt::App::ToForeground();
+}
---
+++ Samples/Notifications/App/CppUnpackagedAppNotifications/CppUnpackagedAppNotifications/Scenario3_PushToastWithAvatar.xaml.cpp
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include <winrt/Microsoft.Windows.AppNotifications.h>
+#include "Scenario3_PushToastWithAvatar.xaml.h"
+#if __has_include("Scenario3_PushToastWithAvatar.g.cpp")
+#include "Scenario3_PushToastWithAvatar.g.cpp"
+#endif
+#include "NotifyUser.h"
+#include "Notifications\ToastWithAvatar.h"
+
+namespace winrt
+{
+    using namespace Microsoft::UI::Xaml;
+    using namespace Microsoft::Windows::AppNotifications;
+}
+
+namespace winrt::CppUnpackagedAppNotifications::implementation
+{
+    MainPage Scenario3_PushToastWithAvatar::rootPage{ nullptr };
+
+    Scenario3_PushToastWithAvatar::Scenario3_PushToastWithAvatar()
+    {
+        InitializeComponent();
+
+        Scenario3_PushToastWithAvatar::rootPage = MainPage::Current();
+    }
+
+    void Scenario3_PushToastWithAvatar::SendToast_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        if (ToastWithAvatar::SendToast())
+        {
+            NotifyUser::ToastSentSuccessfully();
+        }
+        else
+        {
+            NotifyUser::CouldNotSendToast();
+        }
+    }
+}
