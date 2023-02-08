e747d0780b4d00077d4e9e9a6776734a172991fe
Kyaw Thant <kythant@microsoft.com>
2023-02-07 16:41:58 -0800

    update regex

--- UpdateVersions.ps1
+++ UpdateVersions.ps1
@@ -12,8 +12,8 @@ Get-ChildItem -Recurse packages.config -Path $PSScriptRoot | foreach-object {
 }
 
 Get-ChildItem -Recurse *.vcxproj -Path $PSScriptRoot | foreach-object {
-    $newVersionString = '$(NugetPackageDirectory)\Microsoft.WindowsAppSDK.' + $WinAppSDKVersion + '\'
-    $oldVersionString = '$\(NugetPackageDirectory\)\\Microsoft.WindowsAppSDK.[-.0-9a-zA-Z]*\\'
+    $newVersionString = '\Microsoft.WindowsAppSDK.' + $WinAppSDKVersion + '\'
+    $oldVersionString = '\\Microsoft.WindowsAppSDK.[-.0-9a-zA-Z]*\\'
     $content = Get-Content $_.FullName -Raw
     $content = $content -replace $oldVersionString, $newVersionString
     Set-Content -Path $_.FullName -Value $content
