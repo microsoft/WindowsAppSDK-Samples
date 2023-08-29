972f85a60ff6a1c8192a9459be9edbf9f247539c
Jesse Collins <Jesse.Collins@microsoft.com>
2023-08-29 15:11:45 -0700

    Respond to PR feedback

--- Samples/SimpleIslandApp/pch.h
+++ Samples/SimpleIslandApp/pch.h
@@ -4,11 +4,6 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
-// C RunTime Header Files
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
 
 // This tells App.xaml.g.hpp the app will define its own WinMain
 #define DISABLE_XAML_GENERATED_MAIN
