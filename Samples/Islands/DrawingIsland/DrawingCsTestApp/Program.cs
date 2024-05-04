e9f10aa44f16e096a4a16d488c8be86d2675bb03
Manodasan Wignarajah <mawign@microsoft.com>
2024-05-03 17:53:50 -0700

    Get things building

--- Samples/Islands/DrawingIsland/DrawingCsTestApp/Program.cs
+++ Samples/Islands/DrawingIsland/DrawingCsTestApp/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DrawingIslandComponents;
+using System;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Microsoft.UI.Composition.Compositor compositor = new();
+DrawingIsland drawing = new(compositor);
+_= drawing.Island;
