350e1053657e1a4b7f892052da766cf7fe8f0772
Geoffrey Trousdale <getrou@microsoft.com>
2024-05-06 17:55:50 -0400

    Changed LottieIsland to be based of command line argument

---
+++ Samples/Islands/DrawingIsland/DrawingCsTestApp/LottieIslandProgram.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.WinAppSDK.LottieIsland;
+using CommunityToolkit.WinAppSDK.LottieWinRT;
+using Microsoft.UI.Composition;
+using Microsoft.UI.Content;
+
+class LottieIslandProgram
+{
+    public static void CreateLottieIsland(Compositor compositor, DesktopChildSiteBridge siteBridge)
+    {
+        var lottieIsland = LottieContentIsland.Create(compositor);
+        var lottieVisualSource = LottieVisualSourceWinRT.CreateFromString("ms-appx:///Assets/LottieLogo1.json");
+
+        if (lottieVisualSource != null)
+        {
+            lottieVisualSource.AnimatedVisualInvalidated += (sender, args) =>
+            {
+                object? diagnostics = null;
+                IAnimatedVisualFrameworkless? animatedVisual = lottieVisualSource.TryCreateAnimatedVisual(compositor, out diagnostics);
+                if (animatedVisual != null && lottieIsland != null)
+                {
+                    compositor.DispatcherQueue.TryEnqueue(() =>
+                    {
+                        lottieIsland.AnimatedVisual = animatedVisual;
+                        lottieIsland.PlayAsync(0, 1, true);
+                    });
+                }
+            };
+        }
+
+        siteBridge.Connect(lottieIsland.Island);
+    }
+}
--- Samples/Islands/DrawingIsland/DrawingCsTestApp/Program.cs
+++ Samples/Islands/DrawingIsland/DrawingCsTestApp/Program.cs
@@ -28,38 +28,21 @@ window.Show();
 
 var compositor = new Compositor();
 
-// DrawingIsland
-//var drawing = new DrawingIsland(compositor);
-
-//var siteBridge = DesktopChildSiteBridge.Create(compositor, window.Id);
-//siteBridge.ResizePolicy = ContentSizePolicy.ResizeContentToParentWindow;
-//siteBridge.Show();
-//siteBridge.Connect(drawing.Island);
-
-// LottieIsland
-var lottieIsland = LottieContentIsland.Create(compositor);
-var lottieVisualSource = LottieVisualSourceWinRT.CreateFromString("ms-appx:///Assets/LottieLogo1.json");
-if (lottieVisualSource != null)
-{
-    lottieVisualSource.AnimatedVisualInvalidated += (sender, args) =>
-        {
-            object? diagnostics = null;
-            IAnimatedVisualFrameworkless? animatedVisual = lottieVisualSource.TryCreateAnimatedVisual(compositor, out diagnostics);
-            if (animatedVisual != null && lottieIsland != null)
-            {
-                compositor.DispatcherQueue.TryEnqueue(() =>
-                {
-                    lottieIsland.AnimatedVisual = animatedVisual;
-                    lottieIsland.PlayAsync(0, 1, true);
-                });
-            }
-        };
-}
-
 var siteBridge = DesktopChildSiteBridge.Create(compositor, window.Id);
 siteBridge.ResizePolicy = ContentSizePolicy.ResizeContentToParentWindow;
 siteBridge.Show();
-siteBridge.Connect(lottieIsland.Island);
+
+if (args.Contains("Lottie"))
+{
+    // LottieIsland
+    LottieIslandProgram.CreateLottieIsland(compositor, siteBridge);
+}
+else
+{
+    // DrawingIsland
+    var drawing = new DrawingIsland(compositor);
+    siteBridge.Connect(drawing.Island);
+}
 
 queue.RunEventLoop();
 
