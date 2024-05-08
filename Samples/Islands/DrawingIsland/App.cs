b481b8e6a659ca132c1304b41821ae0c4124079b
Jesse Collins <jecollin@microsoft.com>
2024-05-07 17:06:52 -0700

    Add Calc project from WPF-Samples, with a DrawingIsland

---
+++ Samples/Islands/DrawingIsland/App.cs
@@ -0,0 +1,29 @@
+﻿// // Copyright (c) Microsoft. All rights reserved.
+// // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Windows;
+using Microsoft.UI.Dispatching;
+
+namespace CalculatorDemo
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _dispacherQueueController = DispatcherQueueController.CreateOnCurrentThread();
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            _dispacherQueueController?.ShutdownQueue();
+            _dispacherQueueController = null;
+        }
+
+        DispatcherQueueController? _dispacherQueueController;
+    }
+}
---
+++ Samples/Islands/DrawingIsland/WpfIslandHost.cs
@@ -0,0 +1,65 @@
+﻿using DrawingIslandComponents;
+using Microsoft.UI.Content;
+using Microsoft.UI.Dispatching;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using Windows.UI;
+
+namespace CalculatorDemo
+{
+    internal class WpfIslandHost : HwndHost
+    {
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            _compositor = new Microsoft.UI.Composition.Compositor();
+
+            _desktopChildSiteBridge = Microsoft.UI.Content.DesktopChildSiteBridge.Create(
+                _compositor,
+                new Microsoft.UI.WindowId((ulong)hwndParent.Handle));
+
+            //SetupGreenRectangleIsland();
+            SetupDrawingIsland();
+
+            return new HandleRef(null, (nint)_desktopChildSiteBridge.WindowId.Value);
+        }
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            _desktopChildSiteBridge?.Dispose();
+            _desktopChildSiteBridge = null;
+        }
+
+        private void SetupDrawingIsland()
+        {
+            _desktopChildSiteBridge.Connect(new DrawingIsland(_compositor).Island);
+        }
+
+        private void SetupGreenRectangleIsland()
+        {
+            _root = _compositor.CreateContainerVisual();
+
+            var child = _compositor.CreateSpriteVisual();
+            child.Brush = _compositor.CreateColorBrush(Color.FromArgb(0xFF, 0x00, 0xCC, 0x00));
+            child.Size = new Vector2(400, 400);
+
+            _root.Children.InsertAtTop(child);
+
+            _island = ContentIsland.Create(_root);
+            _island.RequestSize(new Vector2(200, 200));
+
+            _desktopChildSiteBridge.Connect(_island);
+        }
+
+        Microsoft.UI.Composition.Compositor? _compositor;
+        Microsoft.UI.Composition.ContainerVisual? _root;
+        Microsoft.UI.Content.DesktopChildSiteBridge? _desktopChildSiteBridge;
+        Microsoft.UI.Content.ContentIsland? _island;
+    }
+}
