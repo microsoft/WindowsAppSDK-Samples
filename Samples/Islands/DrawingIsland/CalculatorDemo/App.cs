6f2e300b5f1fadaf8bd8b297fa9f94c62c86d0c6
Jesse Collins <jecollin@microsoft.com>
2024-05-08 14:00:34 -0700

    Calc: Step 3: Start and shutdown a DispatcherQueue as the App starts and shuts down

--- Samples/Islands/DrawingIsland/CalculatorDemo/App.cs
+++ Samples/Islands/DrawingIsland/CalculatorDemo/App.cs
@@ -1,6 +1,7 @@
 ﻿// // Copyright (c) Microsoft. All rights reserved.
 // // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.UI.Dispatching;
 using System.Windows;
 
 namespace CalculatorDemo
@@ -10,5 +11,21 @@ namespace CalculatorDemo
     /// </summary>
     public partial class App : Application
     {
+        // Many WinAppSDK APIs require a DispatcherQueue to be running on the thread.  We'll start one when the app starts up
+        // and shut it down when the app is finished.
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _dispatcherQueueController = DispatcherQueueController.CreateOnCurrentThread();
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            _dispatcherQueueController?.ShutdownQueue();
+            _dispatcherQueueController = null;
+        }
+
+        DispatcherQueueController? _dispatcherQueueController;
     }
 }
