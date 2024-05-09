09926e1ade20ecf1f0221981cc4e8526e6e549a0
adpa-ms <104169682+adpa-ms@users.noreply.github.com>
2024-05-08 17:45:56 -0700

    Remove unnecessary explicit Close (#347)
    Co-authored-by: Adib Parkar <adibpa@microsoft.com>

--- Samples/Islands/DrawingIsland/DrawingCppTestApp/WinMain.cpp
+++ Samples/Islands/DrawingIsland/DrawingCppTestApp/WinMain.cpp
@@ -24,24 +24,27 @@ int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
     auto compositor = winrt::Compositor();
     auto island = winrt::DrawingIsland(compositor).Island();
 
+    // Associating the AppWindow with the DispatcherQueue on which the ContentIsland is created
+    // will ensure that the ContentIsland is Closed when the AppWindow closes.
     auto window = winrt::AppWindow::Create();
     window.AssociateWithDispatcherQueue(queue);
     window.Closing(
-        [island, queue](auto&&, auto&&)
+        [queue](auto&&, auto&&)
         {
-            island.Close();
+            // Ensure the DispatcherQueue exits the event loop on close.
             queue.EnqueueEventLoopExit();
         });
 
     window.Title(L"Drawing C++ TestApp");
     window.Show();
 
+    // Create a ContentSiteBridge and connect the ContentIsland to it.
     auto siteBridge = winrt::DesktopChildSiteBridge::Create(compositor, window.Id());
     siteBridge.ResizePolicy(winrt::ContentSizePolicy::ResizeContentToParentWindow);
     siteBridge.Show();
     siteBridge.Connect(island);
 
-    // Move initial focus to the island.
+    // Move initial focus to the ContentIsland.
     auto focusNavigationHost = winrt::InputFocusNavigationHost::GetForSiteBridge(siteBridge);
     focusNavigationHost.NavigateFocus(winrt::FocusNavigationRequest::Create(
         winrt::FocusNavigationReason::Programmatic));
