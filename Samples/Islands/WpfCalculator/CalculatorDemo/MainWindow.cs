876d976ac95fed5eb0e7c7b311373a4d906cbb83
Jesse Collins <jecollin@microsoft.com>
2024-05-09 15:37:01 -0700

    More step comments

--- Samples/Islands/WpfCalculator/CalculatorDemo/MainWindow.cs
+++ Samples/Islands/WpfCalculator/CalculatorDemo/MainWindow.cs
@@ -1,7 +1,7 @@
 ﻿// // Copyright (c) Microsoft. All rights reserved.
 // // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#if false // Demo3_Step1_AddWasdk
+#if false // Demo3_Step2_AddCompact
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
 #endif
