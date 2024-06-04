287a2cbb37662d404ee74d3ca0791baf7312d115
Jesse Collins <jecollin@microsoft.com>
2024-06-04 15:08:51 -0700

    Add Lottie

--- Samples/Islands/cs-winui-packaged/WinUIDesktopAppWithIsland/MainWindow.xaml.cs
+++ Samples/Islands/cs-winui-packaged/WinUIDesktopAppWithIsland/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
+using CommunityToolkit.WinAppSDK.LottieIsland;
+using CommunityToolkit.WinAppSDK.LottieWinRT;
 using Microsoft.UI.Composition;
 using Microsoft.UI.Content;
 using Microsoft.UI.Xaml;
@@ -58,9 +61,71 @@ namespace WinUIDesktopAppWithIsland
             _keepAlive.Add(childContentLink);
         }
 
+
+        public void SetupLottie()
+        {
+            ContentIsland parentIsland = this.Content.XamlRoot.TryGetContentIsland();
+            ContainerVisual placementVisual = Compositor.CreateContainerVisual();
+            ChildContentLink childContentLink = ChildContentLink.Create(parentIsland, placementVisual);
+
+            Microsoft.UI.Xaml.Shapes.Rectangle rect = GetNextHostElement();
+            if (rect == null)
+            {
+                return;
+            }
+
+            ElementCompositionPreview.SetElementChildVisual(rect, placementVisual);
+            Vector2 size = rect.ActualSize;
+
+            placementVisual.Size = size;
+            childContentLink.ActualSize = size;
+
+            LottieContentIsland lottieIsland = CreateLottieIsland(placementVisual.Compositor);
+
+            childContentLink.Connect(lottieIsland.Island);
+
+            _keepAlive.Add(childContentLink);
+            _keepAlive.Add(lottieIsland);
+        }
+
+        public static LottieContentIsland CreateLottieIsland(Compositor compositor)
+        {
+            var lottieIsland = LottieContentIsland.Create(compositor);
+            var lottieVisualSource = LottieVisualSourceWinRT.CreateFromString(
+                "ms-appx:///Assets/LottieLogo1.json");
+
+            if (lottieVisualSource != null)
+            {
+                lottieVisualSource.AnimatedVisualInvalidated += (sender, args) =>
+                {
+                    object? diagnostics = null;
+                    IAnimatedVisualFrameworkless? animatedVisual =
+                        lottieVisualSource.TryCreateAnimatedVisual(compositor, out diagnostics);
+
+                    if (animatedVisual != null)
+                    {
+                        // This callback comes back on a different thread, so set the AnimatedVisual on
+                        // the UI thread
+                        compositor.DispatcherQueue.TryEnqueue(async () =>
+                        {
+                            lottieIsland.AnimatedVisual = animatedVisual;
+                            await lottieIsland.PlayAsync(0, 1, true);
+                        });
+                    }
+                };
+            }
+
+            return lottieIsland;
+        }
+
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             SetupHelmet();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetupLottie();
         }
     }
 }
