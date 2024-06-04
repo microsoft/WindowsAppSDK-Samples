2a95f69e8e3c2ac15c65bd97b7113877997575cc
JesseCol <50150435+JesseCol@users.noreply.github.com>
2024-06-04 15:10:31 -0700

    Add app WinUIDesktopAppWithIsland (#357)
    * Initial checkin
    
    * Add SceneNode Helmet
    
    * Add readme.md
    
    * Organize usings and remove Reunion nuget feed
    
    * Add Lottie

---
+++ Samples/Islands/cs-winui-packaged/WinUIDesktopAppWithIsland/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using CommunityToolkit.WinAppSDK.LottieIsland;
+using CommunityToolkit.WinAppSDK.LottieWinRT;
+using Microsoft.UI.Composition;
+using Microsoft.UI.Content;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Hosting;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUIDesktopAppWithIsland
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        List<object> _keepAlive = new List<object>();
+        int idx = 0;
+
+        Microsoft.UI.Xaml.Shapes.Rectangle GetNextHostElement()
+        {
+            if (idx < _rectanglePanel.Children.Count)
+            {
+                return ((Microsoft.UI.Xaml.Shapes.Rectangle)_rectanglePanel.Children[idx++]);
+            }
+
+            return null;
+        }
+
+        public async void SetupHelmet()
+        {
+            ContentIsland parentIsland = this.Content.XamlRoot.TryGetContentIsland();
+            ContainerVisual placementVisual = Compositor.CreateContainerVisual();
+            ChildContentLink childContentLink = ChildContentLink.Create(parentIsland, placementVisual);
+
+            Microsoft.UI.Xaml.Shapes.Rectangle rect = GetNextHostElement();
+            if (rect == null)
+            {
+                return;
+            }
+
+            ElementCompositionPreview.SetElementChildVisual(rect, placementVisual);
+            Vector2 size = rect.ActualSize;
+
+            placementVisual.Size = size;
+            childContentLink.ActualSize = size;
+
+            ContentIsland helmetIsland = await HelmetScenario.CreateIsland(placementVisual.Compositor);
+
+            childContentLink.Connect(helmetIsland);
+
+            _keepAlive.Add(childContentLink);
+        }
+
+
+        public void SetupLottie()
+        {
+            ContentIsland parentIsland = this.Content.XamlRoot.TryGetContentIsland();
+            ContainerVisual placementVisual = Compositor.CreateContainerVisual();
+            ChildContentLink childContentLink = ChildContentLink.Create(parentIsland, placementVisual);
+
+            Microsoft.UI.Xaml.Shapes.Rectangle rect = GetNextHostElement();
+            if (rect == null)
+            {
+                return;
+            }
+
+            ElementCompositionPreview.SetElementChildVisual(rect, placementVisual);
+            Vector2 size = rect.ActualSize;
+
+            placementVisual.Size = size;
+            childContentLink.ActualSize = size;
+
+            LottieContentIsland lottieIsland = CreateLottieIsland(placementVisual.Compositor);
+
+            childContentLink.Connect(lottieIsland.Island);
+
+            _keepAlive.Add(childContentLink);
+            _keepAlive.Add(lottieIsland);
+        }
+
+        public static LottieContentIsland CreateLottieIsland(Compositor compositor)
+        {
+            var lottieIsland = LottieContentIsland.Create(compositor);
+            var lottieVisualSource = LottieVisualSourceWinRT.CreateFromString(
+                "ms-appx:///Assets/LottieLogo1.json");
+
+            if (lottieVisualSource != null)
+            {
+                lottieVisualSource.AnimatedVisualInvalidated += (sender, args) =>
+                {
+                    object? diagnostics = null;
+                    IAnimatedVisualFrameworkless? animatedVisual =
+                        lottieVisualSource.TryCreateAnimatedVisual(compositor, out diagnostics);
+
+                    if (animatedVisual != null)
+                    {
+                        // This callback comes back on a different thread, so set the AnimatedVisual on
+                        // the UI thread
+                        compositor.DispatcherQueue.TryEnqueue(async () =>
+                        {
+                            lottieIsland.AnimatedVisual = animatedVisual;
+                            await lottieIsland.PlayAsync(0, 1, true);
+                        });
+                    }
+                };
+            }
+
+            return lottieIsland;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SetupHelmet();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetupLottie();
+        }
+    }
+}
---
+++ Samples/Islands/cs-winui-packaged/WinUIDesktopAppWithIsland/README.md
@@ -0,0 +1,13 @@
+
+> NOTE: This sample uses Experimental APIs
+
+This sample shows how you can host a ContentIsland in a WinUI Desktop app.
+
+When you click the "Add Helmet" button, it creates a Scene Node island that loads the 3d helmet model and sets up the 3d scene.
+
+It hosts this island in the WinUI Desktop app by using the experimental **ChildContentLink** and **ElementCompositionPreview** APIs.
+
+Here's how it looks:
+
+![Animated gif of a user using the app.](IslandHelmet.gif)
+
