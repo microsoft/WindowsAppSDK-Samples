ab347ef52f0a913545ca9263acf8e26e0451f28b
Jeffrey Stall <TrueRottweiler@users.noreply.github.com>
2024-08-01 09:34:36 +0800

    Port DrawingIsland samples into WASDK 1.6 Exp 2
    This ports the user/jeffstall/drawingisland Samples shown at Build 2024 into the release/experimental branch for WASDK 1.6 Exp 2.

--- Samples/Islands/DrawingIsland/DrawingCppTestApp/pch.h
+++ Samples/Islands/DrawingIsland/DrawingCppTestApp/pch.h
@@ -3,40 +3,30 @@
 
 #pragma once
 
-#include <unknwn.h>
-#include <wil/cppwinrt.h>
-#include <wil/Result.h>
-#include <wil/Resource.h>
-
-#include <winrt/base.h>
+#include <windows.h>
+#ifdef GetCurrentTime
+#undef GetCurrentTime
+#endif
 #include <winrt/Windows.Foundation.h>
-#include "winrt/Windows.Foundation.Collections.h"
+#include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.System.h>
-#include <winrt/Windows.UI.h>
 
+#include <winrt/Microsoft.UI.h>
 #include <winrt/Microsoft.UI.Composition.h>
 #include <winrt/Microsoft.UI.Content.h>
 #include <winrt/Microsoft.UI.Dispatching.h>
 #include <winrt/Microsoft.UI.Input.h>
 #include <winrt/Microsoft.UI.Windowing.h>
 
-// UIA automation related headers
-#include <uiautomationcore.h>
-#include <uiautomationcoreapi.h>
-#include <UIAutomationClient.h>
+#include "winrt/DrawingIslandComponents.h"
 
 namespace winrt
 {
-    using namespace Windows::Foundation;
-    using namespace Windows::Foundation::Collections;
-    using namespace Windows::Foundation::Numerics;
-    using namespace Windows::System;
-    using namespace Windows::UI;
-
     using namespace Microsoft::UI::Composition;
     using namespace Microsoft::UI::Content;
     using namespace Microsoft::UI::Dispatching;
     using namespace Microsoft::UI::Input;
     using namespace Microsoft::UI::Windowing;
-}
 
+    using namespace DrawingIslandComponents;
+}
