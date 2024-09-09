3735f3ae81ffebbbca630f493924f0367dd9ed61
Sreeja Bhattacharya <Sreeja.Bhattacharya@microsoft.com>
2024-09-09 10:30:11 +0530

    Added the cpp sample, and resolved comments

---
+++ Samples/Input/cpp-winui/MainWindow.xaml.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::cpp_winui::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void MyButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void OnKeyDownHandler(IInspectable const& sender, Microsoft::UI::Xaml::Input::KeyRoutedEventArgs const& e);
+        void OnKeyUpHandler(IInspectable const& sender, Microsoft::UI::Xaml::Input::KeyRoutedEventArgs const& e);
+        void OnCharacterReceivedHandler(IInspectable const& sender, Microsoft::UI::Xaml::Input::CharacterReceivedRoutedEventArgs const& e);
+        void OnCtrlRInvoked(Microsoft::UI::Xaml::Input::KeyboardAccelerator const& sender, Microsoft::UI::Xaml::Input::KeyboardAcceleratorInvokedEventArgs e);
+    };
+}
+
+namespace winrt::cpp_winui::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
--- Samples/Input/cs-winui/KeyboardShortcutManager.xaml.cs
+++ Samples/Input/cs-winui/KeyboardShortcutManager.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Input;
