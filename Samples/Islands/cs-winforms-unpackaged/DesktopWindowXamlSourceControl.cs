34455aef8861fb07509dd61c640c6b3efced6b2a
JesseCol <50150435+JesseCol@users.noreply.github.com>
2024-09-18 08:29:23 -0700

    Basics are working

---
+++ Samples/Islands/cs-winforms-unpackaged/DesktopWindowXamlSourceControl.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Hosting;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.UI.Xaml;
+
+namespace WinFormsWithIsland
+{
+    public partial class DesktopWindowXamlSourceControl : System.Windows.Forms.Control
+    {
+        public DesktopWindowXamlSourceControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnGotFocus(EventArgs e)
+        {
+            if (_desktopWindowXamlSource != null)
+            {
+                bool isShiftPressed = ((Form.ModifierKeys & Keys.Shift) != 0);
+                var reason = isShiftPressed ? Microsoft.UI.Xaml.Hosting.XamlSourceFocusNavigationReason.Last : Microsoft.UI.Xaml.Hosting.XamlSourceFocusNavigationReason.First;
+                var request = new XamlSourceFocusNavigationRequest(reason);
+                _desktopWindowXamlSource.NavigateFocus(request);
+            }
+        }
+
+        private void InitializeDesktopWindowXamlSource()
+        {
+            _desktopWindowXamlSource = new Microsoft.UI.Xaml.Hosting.DesktopWindowXamlSource();
+
+            _desktopWindowXamlSource.Initialize(new WindowId((ulong)this.Handle));
+
+            _desktopWindowXamlSource.TakeFocusRequested += (s, e) => {
+                if (e.Request.Reason == Microsoft.UI.Xaml.Hosting.XamlSourceFocusNavigationReason.First)
+                {
+                    WindowsAppSdkHelper.FocusNextFocusableControl(this.Parent, this, true);
+                }
+                else if (e.Request.Reason == Microsoft.UI.Xaml.Hosting.XamlSourceFocusNavigationReason.Last)
+                {
+                    WindowsAppSdkHelper.FocusNextFocusableControl(this.Parent, this, false);
+                }
+            };
+
+            _desktopWindowXamlSource.SiteBridge.MoveAndResize(new Windows.Graphics.RectInt32(0, 0, this.Width, this.Height));
+
+            _frame = new Frame();
+            _desktopWindowXamlSource.Content = _frame;
+
+            _desktopWindowXamlSource.SystemBackdrop = new DesktopAcrylicBackdrop();
+
+            if (_content != null)
+            {
+                _frame.Content = _content;
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (_desktopWindowXamlSource != null)
+            {
+                _desktopWindowXamlSource.SiteBridge.MoveAndResize(new Windows.Graphics.RectInt32(0, 0, this.Width, this.Height));
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            if (this.DesignMode)
+            {
+                base.OnPaint(pe);
+                pe.Graphics.FillRectangle(Brushes.LightGray, this.ClientRectangle);
+                pe.Graphics.DrawString("DesktopWindowXamlSourceControl", this.Font, Brushes.Black, 10, 10);
+            }
+            else if (_desktopWindowXamlSource == null)
+            {
+                InitializeDesktopWindowXamlSource();
+            }
+        }
+
+        public FrameworkElement? Content
+        {
+            get
+            {
+                return _content;
+            }
+            set
+            {
+                _content = value;
+                if (_frame != null)
+                {
+                    _frame.Content = _content;
+                }
+            }
+        }
+
+        FrameworkElement? _content;
+        Frame? _frame;
+        DesktopWindowXamlSource? _desktopWindowXamlSource;
+    }
+}
--- Samples/Islands/cs-winforms-unpackaged/Form1.Designer.cs
+++ Samples/Islands/cs-winforms-unpackaged/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            desktopWindowXamlSourceControl1 = new WinFormsWithIsland.DesktopWindowXamlSourceControl();
+            winFormsButton2 = new Button();
+            winFormsButton1 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(desktopWindowXamlSourceControl1);
+            panel1.Controls.Add(winFormsButton2);
+            panel1.Controls.Add(winFormsButton1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(20);
+            panel1.Size = new Size(755, 445);
+            panel1.TabIndex = 0;
+            // 
+            // desktopWindowXamlSourceControl1
+            // 
+            desktopWindowXamlSourceControl1.Dock = DockStyle.Fill;
+            desktopWindowXamlSourceControl1.Location = new Point(20, 80);
+            desktopWindowXamlSourceControl1.Name = "desktopWindowXamlSourceControl1";
+            desktopWindowXamlSourceControl1.Size = new Size(715, 285);
+            desktopWindowXamlSourceControl1.TabIndex = 1;
+            desktopWindowXamlSourceControl1.Text = "desktopWindowXamlSourceControl1";
+            // 
+            // winFormsButton2
+            // 
+            winFormsButton2.Dock = DockStyle.Bottom;
+            winFormsButton2.Location = new Point(20, 365);
+            winFormsButton2.Name = "winFormsButton2";
+            winFormsButton2.Size = new Size(715, 60);
+            winFormsButton2.TabIndex = 2;
+            winFormsButton2.Text = "WinForms Button 2";
+            winFormsButton2.UseVisualStyleBackColor = true;
+            // 
+            // winFormsButton1
+            // 
+            winFormsButton1.Dock = DockStyle.Top;
+            winFormsButton1.Location = new Point(20, 20);
+            winFormsButton1.Margin = new Padding(0);
+            winFormsButton1.Name = "winFormsButton1";
+            winFormsButton1.Size = new Size(715, 60);
+            winFormsButton1.TabIndex = 0;
+            winFormsButton1.Text = "WinForms Button 1";
+            winFormsButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(755, 445);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private WinFormsWithIsland.DesktopWindowXamlSourceControl desktopWindowXamlSourceControl1;
+        private Button winFormsButton2;
+        private Button winFormsButton1;
     }
 }
