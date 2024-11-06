d8ebfa935c6c2081d6097ca35522e74e287c2a1f
Akanksha Patel <akanpatel22@gmail.com>
2024-11-06 06:18:32 +0530

    OAuth cpp sample app

---
+++ Samples/OAuth/cpp-winui-packaged/TestOAuthInCPlusPlus/App.xaml.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::TestOAuthInCPlusPlus::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
+        static HWND WindowHandle() noexcept;
+        static winrt::Microsoft::UI::Xaml::Window Window() noexcept;
+
+        static inline winrt::Microsoft::UI::Xaml::Window window{ nullptr };
+        static inline HWND windowHandle{ nullptr };
+    };
+}
