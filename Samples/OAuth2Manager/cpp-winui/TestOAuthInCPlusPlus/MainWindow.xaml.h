4df8d72e53fa8fd179bb914bb21728c182d7f1c4
Akanksha Patel <akanpatel22@gmail.com>
2024-12-06 17:00:24 +0530

    OAuth2Manager sample for WinAppSDK 1.7 (#398)
    * OAuth2Manager sample
    
    * Added copyright statement
    
    * Review comments
    
    * Checking the authority if protocol activation is for oauth redirect and then call CompleteAuthRequest

---
+++ Samples/OAuth2Manager/cpp-winui/TestOAuthInCPlusPlus/MainWindow.xaml.h
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::TestOAuthInCPlusPlus::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+        winrt::Windows::Foundation::IAsyncAction implicitGrantWithRedirectButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        winrt::Windows::Foundation::IAsyncAction authCodeGrantWithRedirectButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void OnUriCallback(winrt::Windows::Foundation::Uri const& responseUri);
+
+    private:
+        winrt::Microsoft::UI::WindowId parentWindowId;
+        Microsoft::UI::Dispatching::DispatcherQueue dispatcherQueue{nullptr};
+    };
+}
+
+namespace winrt::TestOAuthInCPlusPlus::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
