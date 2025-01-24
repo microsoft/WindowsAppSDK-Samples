780535a510775d912ab9a6fadea1bc2de12c4b0b
Satwik Kumar Sharma <satwiksharma@microsoft.com>
2025-01-24 20:42:09 +0530

    Added sample for WinAppSDK BadgeNotification

---
+++ Samples/Notifications/Badge/README.md
@@ -0,0 +1,48 @@
+---
+page_type: sample  
+languages:  
+- cppwinrt  
+- cpp  
+products:  
+- windows  
+- windows-app-sdk  
+name: "BadgeNotification Sample"  
+urlFragment: BadgeNotificationSample  
+description: "Demonstrates how to use BadgeNotification API in WinAppSDK, showcasing how to integrate BadgeNotification functionality into desktop apps."  
+extendedZipContent:  
+- path: LICENSE  
+  target: LICENSE  
+---  
+
+
+# Badge Notification Sample Application
+
+This sample demonstrates how to use the **BadgeNotification** API in a **WinUI3** app. It shows how to use the BadgeNotification API to integrate badge notifications within a desktop application. The sample highlights setting badge notifications, handling updates, and displaying the badge on the app's taskbar icon.
+> **Note**: This sample is targeted and tested for the **Windows App SDK 1.7 Experimental 1** and **Visual Studio 2022**.
+
+
+## Features
+
+This sample demonstrates:
+
+- **Numeric Badge**: Displaying a numeric badge on the app's taskbar icon.
+- **Glyph Badge**: Displaying a glyph badge on the app's taskbar icon.
+
+
+
+## Prerequisites
+
+
+* See [System requirements for Windows app development](https://docs.microsoft.com/windows/apps/windows-app-sdk/system-requirements).
+* Make sure that your development environment is set up correctly&mdash;see [Install tools for developing apps for Windows 10 and Windows 11](https://docs.microsoft.com/windows/apps/windows-app-sdk/set-up-your-development-environment).
+* The C# sample requires Visual Studio 2022 and .NET 6.
+
+## Building and Running the Sample
+
+* Open the solution file (`.sln`) in Visual Studio.
+* Press Ctrl+Shift+B, or select **Build** \> **Build Solution**.
+
+
+## Related Documentation and Code Samples
+* [Windows App SDK](https://docs.microsoft.com/windows/apps/windows-app-sdk/)
+* [BadgeNotification API Spec Documentation](https://github.com/microsoft/WindowsAppSDK/blob/main/specs/BadgeNotifications/BadgeNotifications-spec.md)
---
+++ Samples/Notifications/Badge/cpp-winui/Scenario2_GlyphBadge.xaml.cpp
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include "Scenario2_GlyphBadge.xaml.h"
+#if __has_include("Scenario2_GlyphBadge.g.cpp")
+#include "Scenario2_GlyphBadge.g.cpp"
+#include <winrt/Microsoft.Windows.BadgeNotifications.h>
+#endif
+
+using namespace winrt;
+
+namespace winrt
+{
+    using namespace Microsoft::UI::Xaml;
+    using namespace Microsoft::UI::Xaml::Controls;
+    using namespace Windows::Foundation;
+    using namespace Microsoft::Windows::BadgeNotifications;
+}
+
+namespace winrt::cpp_winui::implementation
+{
+    MainPage Scenario2_GlyphBadge::rootPage{ nullptr };
+
+    Scenario2_GlyphBadge::Scenario2_GlyphBadge()
+    {
+        InitializeComponent();
+        Scenario2_GlyphBadge::rootPage = MainPage::Current();
+    }
+
+    void Scenario2_GlyphBadge::showBadge(winrt::BadgeNotificationGlyph glyphValue) {
+        try {
+            winrt::BadgeNotificationManager::Current().SetBadgeAsGlyph(glyphValue);
+            rootPage.NotifyUser(L"Badge Notification Displayed Successfully.", InfoBarSeverity::Informational);
+        }
+        catch (winrt::hresult_error const& ex) {
+            rootPage.NotifyUser(L"An error occurred: " + ex.message(), InfoBarSeverity::Informational);
+        }
+    }
+
+    void Scenario2_GlyphBadge::activityBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Activity);
+    }
+
+    void Scenario2_GlyphBadge::alarmBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Alarm);
+    }
+
+    void Scenario2_GlyphBadge::alertBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Alert);
+    }
+
+    void Scenario2_GlyphBadge::attentionBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Attention);
+    }
+
+    void Scenario2_GlyphBadge::availableBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Available);
+    }
+
+    void Scenario2_GlyphBadge::awayBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Away);
+    }
+
+    void Scenario2_GlyphBadge::busyBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Busy);
+    }
+
+    void Scenario2_GlyphBadge::errorBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Error);
+    }
+
+    void Scenario2_GlyphBadge::newMessageBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::NewMessage);
+    }
+
+    void Scenario2_GlyphBadge::pausedBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Paused);
+    }
+
+    void Scenario2_GlyphBadge::playingBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Playing);
+    }
+
+    void Scenario2_GlyphBadge::unavailableBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        showBadge(winrt::BadgeNotificationGlyph::Unavailable);
+    }
+
+    void Scenario2_GlyphBadge::clearBadge_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        winrt::BadgeNotificationManager::Current().ClearBadge();
+    }
+}
