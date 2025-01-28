8bfe5974fa3519856b15a73d00f35c88d0c13011
SatwikKrSharma <68874478+SatwikKrSharma@users.noreply.github.com>
2025-01-28 17:13:31 +0530

    Added sample for WinAppSDK BadgeNotification (#403)
    * Added sample for WinAppSDK BadgeNotification
    
    * Added C# sample for WinAppSDK BadgeNotification
    
    * created a common method for all the Glyph buttons
    
    ---------
    
    Co-authored-by: Satwik Kumar Sharma <satwiksharma@microsoft.com>

---
+++ Samples/Notifications/Badge/cpp-winui/SampleConfiguration.cpp
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+#include "pch.h"
+#include "SampleConfiguration.h"
+#include "MainPage.xaml.h" 
+
+namespace winrt
+{
+    using namespace Microsoft::UI::Xaml;
+    using namespace Windows::Foundation::Collections;
+}
+
+namespace winrt::cpp_winui
+{
+    IVector<Scenario> implementation::MainPage::scenariosInner = single_threaded_observable_vector<Scenario>(
+        {
+            Scenario{ L"Numeric Badge", hstring(name_of<cpp_winui::Scenario1_NumericBadge>())},
+            Scenario{ L"Glyph Badge", hstring(name_of<cpp_winui::Scenario2_GlyphBadge>())}
+        });
+
+    hstring SampleConfig::FeatureName{ L"cpp_winui" };
+    ElementTheme SampleConfig::CurrentTheme{ ElementTheme::Default };
+}
---
+++ Samples/Notifications/Badge/cpp-winui/Scenario2_GlyphBadge.xaml.cpp
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include "Scenario2_GlyphBadge.xaml.h"
+#if __has_include("Scenario2_GlyphBadge.g.cpp")
+#include "Scenario2_GlyphBadge.g.cpp"
+#include <winrt/Microsoft.Windows.BadgeNotifications.h>
+#endif
+
+using namespace winrt;
+
+namespace winrt
+{
+    using namespace Microsoft::UI::Xaml;
+    using namespace Microsoft::UI::Xaml::Controls;
+    using namespace Windows::Foundation;
+    using namespace Microsoft::Windows::BadgeNotifications;
+}
+
+namespace winrt::cpp_winui::implementation
+{
+    MainPage Scenario2_GlyphBadge::rootPage{ nullptr };
+
+    Scenario2_GlyphBadge::Scenario2_GlyphBadge()
+    {
+        InitializeComponent();
+        Scenario2_GlyphBadge::rootPage = MainPage::Current();
+    }
+
+    void Scenario2_GlyphBadge::badgeButton_Click(IInspectable const& sender, RoutedEventArgs const& e) {
+        auto button = sender.as<Button>();
+        auto tag = button.Tag().as<hstring>();
+
+        if (tag == L"Clear") {
+            winrt::BadgeNotificationManager::Current().ClearBadge();
+            rootPage.NotifyUser(L"Badge Notification Cleared Successfully.", InfoBarSeverity::Informational);
+            return;
+        }
+
+        static const std::map<hstring, winrt::BadgeNotificationGlyph> badgeMap = {
+            { L"Activity", winrt::BadgeNotificationGlyph::Activity },
+            { L"Alarm", winrt::BadgeNotificationGlyph::Alarm },
+            { L"Alert", winrt::BadgeNotificationGlyph::Alert },
+            { L"Attention", winrt::BadgeNotificationGlyph::Attention },
+            { L"Available", winrt::BadgeNotificationGlyph::Available },
+            { L"Away", winrt::BadgeNotificationGlyph::Away },
+            { L"Busy", winrt::BadgeNotificationGlyph::Busy },
+            { L"Error", winrt::BadgeNotificationGlyph::Error },
+            { L"NewMessage", winrt::BadgeNotificationGlyph::NewMessage },
+            { L"Paused", winrt::BadgeNotificationGlyph::Paused },
+            { L"Playing", winrt::BadgeNotificationGlyph::Playing },
+            { L"Unavailable", winrt::BadgeNotificationGlyph::Unavailable }
+        };
+
+        auto it = badgeMap.find(tag);
+        if (it != badgeMap.end()) {
+            winrt::BadgeNotificationManager::Current().SetBadgeAsGlyph(it->second);
+            rootPage.NotifyUser(L"Badge Notification Displayed Successfully.", InfoBarSeverity::Informational);
+        }
+
+        return;
+    }
+}
