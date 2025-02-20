7eef79655e26dad9b116ddf3128a09d94c864fba
Gordon Lam <73506701+yeelam-gordon@users.noreply.github.com>
2025-02-20 09:27:18 +0800

    Fix unpackaged app debugging issue report at #406 -  StorageFile.GetFileFromApplicationUriAsync doesn't support in unpackaged app (#407)
    * Fix StorageFile cannot load in Unpackaged App situation.
    
    * Rename the pub file, given we don't add 10 as part of win10 already in any latest sample, moreover build warning show cannot find the win-arm64.xml given the csproj define the name without 10 as well.
    
    * Update Readme.md to mention how to build or  dotnet run as unpackaged app as example.
    
    * Remove the nuget.config given we don't need it, and we cannot point to public nuget feed.

--- Samples/WindowsCopilotRuntime/cs-winui/Util/StorageFileExtensions.cs
+++ Samples/WindowsCopilotRuntime/cs-winui/Util/StorageFileExtensions.cs
@@ -1,24 +1,64 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace WindowsCopilotRuntimeSample.Util;
 
 internal static class StorageFileExtensions
 {
-    public static async Task<string> ReadFromStorageFileAsync(this string filepath)
+    private static bool IsPackagedApp = (Environment.GetEnvironmentVariable("PACKAGED_PRODUCT_ID") != null);
+
+    internal static async Task<string> ReadTextAsync(this string filepath)
     {
-        if (filepath == null)
+        if (string.IsNullOrWhiteSpace(filepath))
         {
             return string.Empty;
         }
-        var uri = new Uri("ms-appx:///" + filepath);
-        StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(uri);
 
-        string content = await FileIO.ReadTextAsync(file);
-        return content;
+        if (IsPackagedApp)
+        {
+            StorageFile file = await CreateStorageFile(filepath);
+            string content = await FileIO.ReadTextAsync(file);
+            return content;
+        }
+        else
+        {
+            string filePath = CombineWithBasePath(filepath);
+            string content = await File.ReadAllTextAsync(filePath);
+            return content;
+        }
     }
 
+    internal static async Task<IRandomAccessStream> CreateStreamAsync(this string filepath)
+    {
+        if (string.IsNullOrWhiteSpace(filepath))
+        {
+            return MemoryStream.Null.AsRandomAccessStream();
+        }
+
+        if (IsPackagedApp)
+        {
+            StorageFile file = await CreateStorageFile(filepath);
+            return await file.OpenAsync(FileAccessMode.Read);
+        }
+        else
+        {
+            string filePath = CombineWithBasePath(filepath);
+            return File.OpenRead(filePath).AsRandomAccessStream();
+        }
+    }
+    private static Task<StorageFile> CreateStorageFile(string filepath)
+    {
+        var uri = new Uri("ms-appx:///" + filepath);
+        return StorageFile.GetFileFromApplicationUriAsync(uri).AsTask();
+    }
+
+    private static string CombineWithBasePath(string filepath)
+    {
+        return Path.Combine(AppContext.BaseDirectory, filepath);
+    }
 }
