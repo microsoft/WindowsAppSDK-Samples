d35c7089739f7b31bc7593fa138dbc377a978448
Jon Wiswall <jonwis@microsoft.com>
2025-03-17 22:15:46 -0700

    Start using text generator

--- Samples/WindowsCopilotRuntime/cpp-cmake/main.cpp
+++ Samples/WindowsCopilotRuntime/cpp-cmake/main.cpp
@@ -2,8 +2,45 @@
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Microsoft.Windows.AI.Generative.h>
 
-int wmain()
+int wmain(int argc, wchar_t* argv[])
 {
     // Initialize the Windows Runtime
     winrt::init_apartment();
+    std::wstring prompt = L"You are a clever storyteller. I want to hear a story about a dragon who says: ";
+
+    if (argc < 2)
+    {
+        wprintf(L"Usage: %s <prompt>\n", argv[0]);
+        wprintf(L"Using a dummy prompt...\n");
+        prompt += L"Where are my treasures?";
+    }
+    else
+    {
+        prompt += argv[1];
+    }
+
+    // Create an instance of the LanguageModel
+    if (!winrt::Microsoft::Windows::AI::Generative::LanguageModel::IsAvailable())
+    {
+        wprintf(L"Fetching language model...\n");
+        winrt::Microsoft::Windows::AI::Generative::LanguageModel::MakeAvailableAsync().get();
+    }
+
+    // Try again
+    if (!winrt::Microsoft::Windows::AI::Generative::LanguageModel::IsAvailable())
+    {
+        wprintf(L"Language model is still not available.\n");
+        return 1;
+    }
+
+    // Instantiate the text completion generator
+    auto languageModel = winrt::Microsoft::Windows::AI::Generative::LanguageModel::CreateAsync().get();
+    auto options = winrt::Microsoft::Windows::AI::Generative::LanguageModelOptions();
+    options.Skill(winrt::Microsoft::Windows::AI::Generative::LanguageModelSkill::General);
+    options.Temp(0.7f);
+    auto response = languageModel.GenerateResponseAsync(options, prompt).get();
+    wprintf(L"Status: %d\n", static_cast<int>(response.Status()));
+    wprintf(L"Response: %s\n", response.Response().c_str());
+
+    return 0;
 }
