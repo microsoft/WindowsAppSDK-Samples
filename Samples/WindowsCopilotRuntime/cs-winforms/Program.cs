41066bdb2de02f134ee31cbec41108b1202291ad
ssparach <>
2025-03-24 11:40:54 -0700

    save state

--- Samples/WindowsCopilotRuntime/cs-winforms/Program.cs
+++ Samples/WindowsCopilotRuntime/cs-winforms/Program.cs
@@ -1,4 +1,4 @@
-namespace WinFormsApp1
+﻿namespace WindowsCopilotRuntimeSample
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace WinFormsApp1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
