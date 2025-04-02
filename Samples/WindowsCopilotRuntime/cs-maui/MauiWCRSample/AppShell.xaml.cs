cc5917f006612cd0fad1a9b71d7e91e8d99ea042
Mike Crider <45052236+codendone@users.noreply.github.com>
2025-04-02 10:29:30 -0700

    Add a sample of a .NET MAUI app using WCR (#431)
    * base project plus ImageScaler demo and text generation demo
    
    * Add README
    
    * Move project up one folder
    
    * Added details on the package reference and target framework.
    
    * PR feedback.

---
+++ Samples/WindowsCopilotRuntime/cs-maui/MauiWCRSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiWindowsCopilotRuntimeSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
