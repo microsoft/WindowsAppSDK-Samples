c52a362f6e038bf54aa5337f7d5f7b10f6914d1c
pratiksinha07 <89442836+pratiksinha07@users.noreply.github.com>
2025-04-07 22:28:38 +0530

    Added Sample for Image Object Remover API in WinAppSDK-Samples (#435)
    * Added initial level changes for ImageObjectRemover model
    
    * Added further changes for ImageObjectRemover Sample
    
    * Incorporated changes regarding the release nuget
    
    * Added few changes to keep a check on ImageBuffer issue and added additional details about inputs.
    
    * Changed the Image in the Assets and corrected the markdown

---
+++ Samples/WindowsCopilotRuntime/cs-winui/Models/ImageObjectRemoverModel.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using WindowsCopilotRuntimeSample.Models.Contracts;
+using Microsoft.Graphics.Imaging;
+using Microsoft.Windows.Management.Deployment;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Microsoft.Windows.AI;
+
+namespace WindowsCopilotRuntimeSample.Models;
+
+internal class ImageObjectRemoverModel : IModelManager
+{
+    private ImageObjectRemover? _session;
+
+    private ImageObjectRemover Session => _session ?? throw new InvalidOperationException("Image Object Remover session was not created yet");
+
+    public async Task CreateModelSessionWithProgress(IProgress<double> progress, CancellationToken cancellationToken = default)
+    {
+        if (ImageObjectRemover.GetReadyState() == AIFeatureReadyState.EnsureNeeded)
+        {
+            var objectRemoveDeploymentOperation = ImageObjectRemover.EnsureReadyAsync();
+            objectRemoveDeploymentOperation.Progress = (_, packageDeploymentProgress) =>
+            {
+                progress.Report(packageDeploymentProgress);
+            };
+            using var _ = cancellationToken.Register(() => objectRemoveDeploymentOperation.Cancel());
+            await objectRemoveDeploymentOperation;
+        }
+        else
+        {
+            progress.Report(100.0);
+        }
+        _session = await ImageObjectRemover.CreateAsync();
+    }
+
+    public SoftwareBitmap RemoveFromSoftwareBitmap(SoftwareBitmap inputImage, SoftwareBitmap inputMask)
+    {
+        return Session.RemoveFromSoftwareBitmap(inputImage, inputMask);
+    }
+}
