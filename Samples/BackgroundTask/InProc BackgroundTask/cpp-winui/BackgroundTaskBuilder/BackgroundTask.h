37af04ab84b34a7b38a97e73755315a4a8a40dd9
Godly T.Alias <godlytalias@yahoo.co.in>
2025-04-08 22:20:11 +0530

    Background Task Samples: Cherry pick from experimental branch (#438)
    * Added sample for WinAppSDK BackgroundTaskBuilder (#392)
    
    * Added sample for WinAppSDK BackgroundTaskBuilder
    
    Signed-off-by: Godly Alias <godlyalias@microsoft.com>
    
    * Added comments for improved readability of the sample (#400)
    
    Signed-off-by: godlytalias <godlytalias@yahoo.co.in>
    
    * InProc BackgroundTask Samples (#413)
    
    * Added InProc BackgroundTask Samples
    
    Signed-off-by: godlytalias <godlytalias@yahoo.co.in>
    
    * Support for trimming in BackgroundTask Samples (#421)
    
    Signed-off-by: godlytalias <godlytalias@yahoo.co.in>
    
    ---------
    
    Signed-off-by: godlytalias <godlytalias@yahoo.co.in>

---
+++ Samples/BackgroundTask/InProc BackgroundTask/cpp-winui/BackgroundTaskBuilder/BackgroundTask.h
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "pch.h"
+#include "MainWindow.g.h"
+
+#define CLSID_BackgroundTask "12345678-1234-1234-1234-1234567890CD"
+namespace winrt::BackgroundTaskBuilder
+{
+    struct __declspec(uuid(CLSID_BackgroundTask))
+    BackgroundTask : implements<BackgroundTask, winrt::Windows::ApplicationModel::Background::IBackgroundTask>
+    {
+        void Run(_In_ winrt::Windows::ApplicationModel::Background::IBackgroundTaskInstance taskInstance);
+    private:
+        void OnCanceled(_In_ winrt::Windows::ApplicationModel::Background::IBackgroundTaskInstance /* taskInstance */, _In_ winrt::Windows::ApplicationModel::Background::BackgroundTaskCancellationReason /* cancelReason */);
+        volatile bool m_cancelRequested = false;
+        winrt::Windows::ApplicationModel::Background::BackgroundTaskDeferral m_deferral = nullptr;
+        Windows::System::Threading::ThreadPoolTimer m_periodicTimer = nullptr;
+        winrt::BackgroundTaskBuilder::IMainWindow m_mainWindow = nullptr;
+        int m_progress{ 0 };
+    };
+
+    struct BackgroundTaskFactory : implements<BackgroundTaskFactory, IClassFactory>
+    {
+        HRESULT __stdcall CreateInstance(_In_opt_ IUnknown* aggregateInterface, _In_ REFIID interfaceId, _Outptr_ VOID** object) noexcept final try
+        {
+            if (aggregateInterface != NULL) {
+                return CLASS_E_NOAGGREGATION;
+            }
+
+            return make<BackgroundTask>().as(interfaceId, object);
+        }
+        CATCH_RETURN();
+
+        HRESULT __stdcall LockServer(BOOL lock) noexcept final try
+        {
+            UNREFERENCED_PARAMETER(lock);
+            return S_OK;
+        }
+        CATCH_RETURN();
+    };
+}
+
---
+++ Samples/BackgroundTask/InProc BackgroundTask/cpp-winui/BackgroundTaskBuilder/RegisterForCOM.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include "RegisterForCOM.h"
+
+using namespace winrt;
+
+namespace winrt::BackgroundTaskBuilder
+{
+    RegisterForCom::~RegisterForCom()
+    {
+        if (ComRegistrationToken != 0)
+        {
+            CoRevokeClassObject(ComRegistrationToken);
+        }
+    }
+
+    hresult RegisterForCom::RegisterBackgroundTaskFactory()
+    {
+        hresult hr;
+        try
+        {
+            com_ptr<IClassFactory> taskFactory = make<BackgroundTaskFactory>();
+
+            check_hresult(CoRegisterClassObject(__uuidof(BackgroundTask),
+                taskFactory.detach(),
+                CLSCTX_LOCAL_SERVER,
+                REGCLS_MULTIPLEUSE,
+                &ComRegistrationToken));
+
+            OutputDebugString(L"COM Registration done");
+            hr = S_OK;
+        }
+        CATCH_RETURN();
+    }
+}
