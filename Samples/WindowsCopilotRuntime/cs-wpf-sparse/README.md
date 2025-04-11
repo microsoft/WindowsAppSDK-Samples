49eed8d071018ad93441626c7b8fa57ccf8d0da6
Kyaw Thant <48363984+kythant@users.noreply.github.com>
2025-04-11 12:51:29 -0700

    CS WPF Sparse Package Sample With WCR (#440)

---
+++ Samples/WindowsCopilotRuntime/cs-wpf-sparse/README.md
@@ -0,0 +1,90 @@
+---
+page_type: sample
+languages:
+- csharp
+products:
+- windows
+- windows-app-sdk
+name: cs-wpf-sparse
+description: Shows how to integrate the Windows Copilot Runtime APIs in a sparse WPF package
+urlFragment: cs-wpf-sparse
+extendedZipContent:
+- path: LICENSE
+  target: LICENSE
+---
+
+## Prerequisites
+- For system requirements, see [System requirements for Windows app development](https://docs.microsoft.com/windows/apps/windows-app-sdk/system-requirements).
+- To ensure your development environment is set up correctly, see [Install tools for developing apps for Windows 10 and Windows 11](https://docs.microsoft.com/windows/apps/windows-app-sdk/set-up-your-development-environment).
+- Running this sample does require a [Windows Copilot + PC](https://learn.microsoft.com/windows/ai/npu-devices/)
+- This sample requires [Windows App SDK 1.7 Experimental3](https://learn.microsoft.com/windows/apps/windows-app-sdk/downloads#windows-app-sdk-17-experimental), which should be installed by Visual Studio when deploying or launching the app.
+
+## Build and Run the sample
+
+This folder contains the cs-wpf-sparse sample which uses sparse package built on top of the WPF
+platform using WCR. BuildSparsePackage.ps1 is provided for the user which closely follows the steps
+from the link below
+
+https://learn.microsoft.com/en-us/windows/apps/desktop/modernize/grant-identity-to-nonpackaged-apps#add-the-package-identity-metadata-to-your-desktop-application-manifest
+
+BuildSparsePackage.ps1 does the following:
+1) Build the solution in the desired platform and configuration provided in the parameters
+ 
+2) Run MakeAppx with the /nv option on the folder containing the AppxManifest
+    (cs-wpf-sparse\WCRforWPF\AppxManifest.xml) 
+    
+    - The /nv flag is required to bypass validation of referenced file paths in the manifest. 
+    
+    - The output folder is set to the output of the binaries when we build the solution regularly
+    (bin\$Platform\$Configuration\net8.0-windows10.0.22621.0) so that the MSIX will be side by side
+    to the executable built in step 1. This placement is necessary because the exe expects the external location of
+    the MSIX to be the same as it's file location.
+
+3) Run SignTool to sign the MSIX
+
+Please have the WindowsAppSDK runtime installed using the installer for 1.7.250127003-experimental3
+at https://learn.microsoft.com/en-us/windows/apps/windows-app-sdk/downloads
+
+Please install the cert in the .user folder to LocalMachine TrustedPeople root. You can either
+launch from the start menu after installing the MSIX in the bin directory or you can directly run
+the exe. 
+
+## Sample Overview
+
+A sparse package allows unpackaged win32 apps to gain package identity without fully
+adopting the MSIX packaging format. This is particularly useful for applications that are not yet
+ready to have all their content inside an MSIX package but still need to use Windows extensibility
+features that require package identity.
+
+### Initialization code
+
+Just doing the steps above does not yet grant the app package identity. The MSIX package still needs to
+be registered with the external location. 
+
+The RegisterSparsePackage() function in cs-wpf-sparse\WCRforWPF\App.xaml.cs does this for us. This
+process is described in the documentation linked above. This will give the app process identity if
+ran from the start menu. However, directly running the executable in the external location still
+does not give the process package identity. This difference in behavior is not noted in the article
+from above.
+
+To allow the resulting app process from directly running the executable to gain identity requires
+this workaround. 
+
+The Startup() code has two different codepaths for whether the process is a packaged or a unpackaged
+process (With or without identity).
+
+If without Identity, RegisterSparsePackage() will be called which registers the package on the
+external path if it hasn't been installed already. Afterwards, RunWithIdentity() will call
+ActivateApplication on the package identity it registered earlier using the ActivationManager.
+
+This will spawn a packaged process of itself which will have identity. The non-identity process will
+then exit. The new packaged process in the startup will hit the codepath for when IsPackageProcess()
+is true and launch MainWindow. 
+
+Note: the first run of the exe run will require a longer startup time because of the time that is
+needed to register the package. 
+
+### Additional Notes
+
+## Related Links
+- [Windows Copilot Runtime Overview](https://learn.microsoft.com/windows/ai/apis/)
---
+++ Samples/WindowsCopilotRuntime/cs-wpf-sparse/WCRforWPF/App.xaml.cs
@@ -0,0 +1,190 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.IO;
+using Windows.Foundation;
+using Windows.Management.Deployment;
+using Windows.ApplicationModel;
+
+namespace WCRforWPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    // All the interpolated native calls and DLLImports go here
+    private static class NativeMethods
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int GetCurrentPackageFullName(ref int packageFullNameLength, [Out] char[] packageFullName);
+        
+        public enum ActivateOptions
+        {
+            None = 0x00000000,  // No flags set
+            DesignMode = 0x00000001,  // The application is being activated for design mode, and thus will not be able to
+                                      // to create an immersive window. Window creation must be done by design tools which
+                                      // load the necessary components by communicating with a designer-specified service on
+                                      // the site chain established on the activation manager.  The splash screen normally
+                                      // shown when an application is activated will also not appear.  Most activations
+                                      // will not use this flag.
+            NoErrorUI = 0x00000002,   // Do not show an error dialog if the app fails to activate.                                
+            NoSplashScreen = 0x00000004,  // Do not show the splash screen when activating the app.
+        }
+
+        public const string CLSID_ApplicationActivationManager_String = "45ba127d-10a8-46ea-8ab7-56ea9078943c";
+        public const string CLSID_IApplicationActivationManager_String = "2e941141-7f97-4756-ba1d-9decde894a3d";
+
+        public static readonly Guid CLSID_ApplicationActivationManager = new Guid(CLSID_ApplicationActivationManager_String);
+        public static readonly Guid CLSID_IApplicationActivationManager = new Guid(CLSID_IApplicationActivationManager_String);
+
+        [ComImport, Guid(CLSID_IApplicationActivationManager_String), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IApplicationActivationManager
+        {
+            // Activates the specified immersive application for the "Launch" contract, passing the provided arguments
+            // string into the application.  Callers can obtain the process Id of the application instance fulfilling this contract.
+            IntPtr ActivateApplication([In] String appUserModelId, [In] String arguments, [In] ActivateOptions options, [Out] out UInt32 processId);
+            IntPtr ActivateForFile([In] String appUserModelId, [In] IntPtr /*IShellItemArray* */ itemArray, [In] String verb, [Out] out UInt32 processId);
+            IntPtr ActivateForProtocol([In] String appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out UInt32 processId);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("ole32.dll", CharSet = CharSet.Unicode, ExactSpelling = true, PreserveSig = false)]
+        public static extern UInt32 CoCreateInstance(
+            [In, MarshalAs(UnmanagedType.LPStruct)] Guid rclsid,
+            IntPtr pUnkOuter,
+            CLSCTX dwClsContext,
+            [In, MarshalAs(UnmanagedType.LPStruct)] Guid riid,
+            [MarshalAs(UnmanagedType.IUnknown)] out object rReturnedComObject);
+
+        [Flags]
+        public enum CLSCTX : uint
+        {
+            CLSCTX_INPROC_SERVER = 0x1,
+            CLSCTX_INPROC_HANDLER = 0x2,
+            CLSCTX_LOCAL_SERVER = 0x4,
+            CLSCTX_INPROC_SERVER16 = 0x8,
+            CLSCTX_REMOTE_SERVER = 0x10,
+            CLSCTX_INPROC_HANDLER16 = 0x20,
+            CLSCTX_RESERVED1 = 0x40,
+            CLSCTX_RESERVED2 = 0x80,
+            CLSCTX_RESERVED3 = 0x100,
+            CLSCTX_RESERVED4 = 0x200,
+            CLSCTX_NO_CODE_DOWNLOAD = 0x400,
+            CLSCTX_RESERVED5 = 0x800,
+            CLSCTX_NO_CUSTOM_MARSHAL = 0x1000,
+            CLSCTX_ENABLE_CODE_DOWNLOAD = 0x2000,
+            CLSCTX_NO_FAILURE_LOG = 0x4000,
+            CLSCTX_DISABLE_AAA = 0x8000,
+            CLSCTX_ENABLE_AAA = 0x10000,
+            CLSCTX_FROM_DEFAULT_CONTEXT = 0x20000,
+            CLSCTX_ACTIVATE_32_BIT_SERVER = 0x40000,
+            CLSCTX_ACTIVATE_64_BIT_SERVER = 0x80000,
+            CLSCTX_INPROC = CLSCTX_INPROC_SERVER | CLSCTX_INPROC_HANDLER,
+            CLSCTX_SERVER = CLSCTX_INPROC_SERVER | CLSCTX_LOCAL_SERVER | CLSCTX_REMOTE_SERVER,
+            CLSCTX_ALL = CLSCTX_SERVER | CLSCTX_INPROC_HANDLER
+        }
+    }
+
+    public App()
+    {
+    }
+
+    // Originally StartupUri="MainWindow.xaml" was used instead of Startup="OnStartUp" in
+    // the App.xaml Application properties and the startup code lived in MainWindow.xaml.cs
+    // But this design presents a flaw where the unpackaged process will show itself before
+    // the package process is able to activate. 
+    //
+    // This could be mitigated using this.hide on the MainWindows startup but the unpackaged
+    // process still showed itself for a split second.
+    //
+    // Using OnStartUp and intializating the MainWindow once we know we are in the packaged process
+    // proved to be a proper solution to avoid this issue.
+    private void OnStartUp(Object sender, StartupEventArgs e)
+    {
+        RestartWithIdentityIfNecessary();
+        InitializeComponent();
+    }
+
+    private async Task RestartWithIdentityIfNecessary()
+    {
+        // If we are in the packaged process, then present the MainWindow
+        if (IsPackagedProcess())
+        {
+            var mainWindow = new MainWindow();
+            MainWindow.Show(); // Show the main window
+            return;
+        }
+
+        // If we are still unpackaged process, then register the MSIX and ActivateApplication
+        Task install = RegisterSparsePackage();
+        await install;
+        RunWithIdentity();
+        Environment.Exit(0);
+    }
+
+    private async Task RegisterSparsePackage()
+    {
+        // We expect the MSIX to be in the same directory as the exe. 
+        string exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        string externalLocation = exePath;
+        string sparsePkgPath = exePath + "\\WCRforWPFSparse.msix";
+
+        Uri externalUri = new Uri(externalLocation);
+        Uri packageUri = new Uri(sparsePkgPath);
+
+        PackageManager packageManager = new PackageManager();
+        int count = packageManager.FindPackagesForUserWithPackageTypes("", "WCRforWPFSparse_k0t3h69cz9sxw", PackageTypes.Main).ToList<Package>().Count;
+        if (count == 0)
+        {
+            //Declare use of an external location
+            var options = new AddPackageOptions();
+            options.ExternalLocationUri = externalUri;
+
+            var deploymentOperation = packageManager.AddPackageByUriAsync(packageUri, options);
+            var deploymentResult = await deploymentOperation;
+
+            if (deploymentOperation.Status == AsyncStatus.Completed)
+            {
+                return;
+            }
+            else
+            {
+                throw new Exception("Package did not register");
+            }
+        }
+    }
+
+    private void RunWithIdentity()
+    {
+        // Activating the packaged process
+        // We should already know our AUMID which depends on the AppxManifest we defined so this can be hardcoded here. 
+        string appUserModelId = "WCRforWPFSparse_k0t3h69cz9sxw!WCRforWPFSparsePkg";
+        if (NativeMethods.CoCreateInstance(
+            NativeMethods.CLSID_ApplicationActivationManager,
+            IntPtr.Zero,
+            NativeMethods.CLSCTX.CLSCTX_LOCAL_SERVER,
+            NativeMethods.CLSID_IApplicationActivationManager,
+            out object applicationActivationManagerAsObject) != 0)
+        {
+            throw new Exception("Failed to create ApplicationActivationManager!");
+        }
+        var applicationActivationManager = (NativeMethods.IApplicationActivationManager)applicationActivationManagerAsObject;
+        applicationActivationManager.ActivateApplication(appUserModelId, null, NativeMethods.ActivateOptions.None, out uint processId);
+    }
+
+    private static bool IsPackagedProcess()
+    {
+        int length = 0;
+        int result = NativeMethods.GetCurrentPackageFullName(ref length, null);
+        if (result == 15700) // APPMODEL_ERROR_NO_PACKAGE
+        {
+            return false;
+        }
+        char[] packageFullName = new char[length];
+        result = NativeMethods.GetCurrentPackageFullName(ref length, packageFullName);
+        return result == 0;
+    }
+}
