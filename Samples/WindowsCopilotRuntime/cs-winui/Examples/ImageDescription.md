ee73e50f939f3d5b328a6d04ce331ac6dd3723a4
Pratik Anand <pratik.preet@gmail.com>
2025-04-15 12:55:07 -0700

    image description better error handling

--- Samples/WindowsCopilotRuntime/cs-winui/Examples/ImageDescription.md
+++ Samples/WindowsCopilotRuntime/cs-winui/Examples/ImageDescription.md
@@ -4,14 +4,5 @@ ImageBuffer imageBuffer = ImageBuffer.CreateCopyFromBitmap(inputImage);
 // Image Description Scenario and Content Filter Options can be chosen as per scenario
 
 ContentFilterOptions filterOptions = new();
-filterOptions.ImageMaxAllowedSeverityLevel.AdultContentLevel = SeverityLevel.Minimum;
-filterOptions.ImageMaxAllowedSeverityLevel.RacyContentLevel = SeverityLevel.Minimum;
-filterOptions.ImageMaxAllowedSeverityLevel.GoryContentLevel = SeverityLevel.Minimum;
-filterOptions.ImageMaxAllowedSeverityLevel.ViolentContentLevel = SeverityLevel.Minimum;
-filterOptions.ResponseMaxAllowedSeverityLevel.Violent = SeverityLevel.Minimum;
-filterOptions.ResponseMaxAllowedSeverityLevel.SelfHarm = SeverityLevel.Minimum;
-filterOptions.ResponseMaxAllowedSeverityLevel.Sexual = SeverityLevel.Minimum;
-filterOptions.ResponseMaxAllowedSeverityLevel.Hate = SeverityLevel.Minimum;
-
 var modelResponse = await Session.DescribeAsync(inputImage, ImageDescriptionKind.BriefDescription, filterOptions);
 return modelResponse.Description;
--- Samples/WindowsCopilotRuntime/cs-winui/Models/ImageDescriptionModel.cs
+++ Samples/WindowsCopilotRuntime/cs-winui/Models/ImageDescriptionModel.cs
@@ -40,16 +40,11 @@ class ImageDescriptionModel  : IModelManager
     public async Task<string> DescribeImage(ImageBuffer inputImage, CancellationToken cancellationToken = default)
     {
         ContentFilterOptions filterOptions = new();
-        filterOptions.ImageMaxAllowedSeverityLevel.AdultContentLevel = SeverityLevel.Minimum;
-        filterOptions.ImageMaxAllowedSeverityLevel.RacyContentLevel = SeverityLevel.Minimum;
-        filterOptions.ImageMaxAllowedSeverityLevel.GoryContentLevel = SeverityLevel.Minimum;
-        filterOptions.ImageMaxAllowedSeverityLevel.ViolentContentLevel = SeverityLevel.Minimum;
-        filterOptions.ResponseMaxAllowedSeverityLevel.Violent = SeverityLevel.Minimum;
-        filterOptions.ResponseMaxAllowedSeverityLevel.SelfHarm = SeverityLevel.Minimum;
-        filterOptions.ResponseMaxAllowedSeverityLevel.Sexual = SeverityLevel.Minimum;
-        filterOptions.ResponseMaxAllowedSeverityLevel.Hate = SeverityLevel.Minimum;
-
         var modelResponse = await Session.DescribeAsync(inputImage, ImageDescriptionKind.BriefDescription, filterOptions);
+        if (modelResponse.Status != ImageDescriptionResultStatus.Complete)
+        {
+            return $"Image description failed with status: {modelResponse.Status}";
+        }
         return modelResponse.Description;
     }
 }
