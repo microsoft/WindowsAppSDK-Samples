c59c53ab38d1c41cd2beb24a51c9e090ce24814f
Will Thant <kythant@microsoft.com>
2025-04-17 22:04:32 +0000

    Wpf docs sample

---
+++ Samples/WindowsCopilotRuntime/wpf-docs/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var readyState = Microsoft.Windows.AI.Generative.LanguageModel.GetReadyState();
+                System.Diagnostics.Debug.WriteLine($"LanguageModel.GetReadyState: {readyState}");
+            }
+            catch
+            {
+            }
+        }
+    }
+}
