da1fb75157f92d6e296df2f5b02a6462eee38f0f
Manodasan Wignarajah <mawign@microsoft.com>
2025-05-04 23:48:31 -0700

    Update samples

--- UpdateVersions.ps1
+++ UpdateVersions.ps1
@@ -1,21 +1,42 @@
 Param(
     [string]$WinAppSDKVersion = ""
+    [string]$NuGetPackagesFolder = ""
 )
 
+$nugetPackageToVersionTable = @{"Microsoft.WindowsAppSDK" = $WinAppSDKVersion}
+if (!($NuGetPackagesFolder -eq ""))
+{
+    [xml]$publicNuspec = Get-Content -Encoding utf8 -Path (Join-Path $NuGetPackagesFolder "Microsoft.WindowsAppSDK\$WinAppSDKVersion\Microsoft.WindowsAppSDK.nuspec")
+    foreach ($dependency in $publicNuspec.package.metadata.dependencies.dependency)
+    {
+        $nugetPackageToVersionTable[$dependency.id] = $dependency.version
+    }
+}
+
 Get-ChildItem -Recurse packages.config -Path $PSScriptRoot | foreach-object {
-    $newVersionString = 'package id="Microsoft.WindowsAppSDK" version="' + $WinAppSDKVersion + '"'
-    $oldVersionString = 'package id="Microsoft.WindowsAppSDK" version="[-.0-9a-zA-Z]*"'
     $content = Get-Content $_.FullName -Raw
-    $content = $content -replace $oldVersionString, $newVersionString
+
+    foreach ($nugetPackageToVersion in $nugetPackageToVersionTable.GetEnumerator())
+    {
+        $newVersionString = 'package id="' + $nugetPackageToVersion.Key + '" version="' + $nugetPackageToVersion.Value + '"'
+        $oldVersionString = 'package id="' + $nugetPackageToVersion.Key + '" version="[-.0-9a-zA-Z]*"'
+        $content = $content -replace $oldVersionString, $newVersionString
+    }
+
     Set-Content -Path $_.FullName -Value $content
     Write-Host "Modified " $_.FullName 
 }
 
 Get-ChildItem -Recurse *.vcxproj -Path $PSScriptRoot | foreach-object {
-    $newVersionString = '\Microsoft.WindowsAppSDK.' + $WinAppSDKVersion + '\'
-    $oldVersionString = '\\Microsoft.WindowsAppSDK.[-.0-9a-zA-Z]*\\'
     $content = Get-Content $_.FullName -Raw
-    $content = $content -replace $oldVersionString, $newVersionString
+
+    foreach ($nugetPackageToVersion in $nugetPackageToVersionTable.GetEnumerator())
+    {
+        $newVersionString = "\$($nugetPackageToVersion.Key)." + $WinAppSDKVersion + '\'
+        $oldVersionString = "\$($nugetPackageToVersion.Key).[0-9][-.0-9a-zA-Z]*\"
+        $content = $content -replace $oldVersionString, $newVersionString
+    }
+
     Set-Content -Path $_.FullName -Value $content
     Write-Host "Modified " $_.FullName 
 }
