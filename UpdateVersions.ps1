d26180971ff49a63b23ce1459afdc0bef2b0e465
Manodasan Wignarajah <mawign@microsoft.com>
2025-05-04 23:48:31 -0700

    Updating samples to reflect package changes

--- UpdateVersions.ps1
+++ UpdateVersions.ps1
@@ -1,21 +1,49 @@
 Param(
-    [string]$WinAppSDKVersion = ""
+    [string]$WinAppSDKVersion = "",
+    [string]$NuGetPackagesFolder = ""
 )
 
+# If the nuget packages folder is specified containing the latest versions,
+# go through them to get the versions of the component packages.
+$nugetPackageToVersionTable = @{"Microsoft.WindowsAppSDK" = $WinAppSDKVersion}
+if (!($NuGetPackagesFolder -eq ""))
+{
+    Get-ChildItem $NuGetPackagesFolder | 
+    Where-Object { $_.Name -like "Microsoft.WindowsAppSDK*" } | 
+    Where-Object { $_.Name -notlike "*.nupkg" } |
+    ForEach-Object { 
+        if ($_.Name -match "^(Microsoft\.WindowsAppSDK\.[a-zA-Z]+)\.([0-9].*)$")
+        {
+            $nugetPackageToVersionTable[$Matches[1]] = $Matches[2]
+            Write-Host "Found $($Matches[1]) - $($Matches[2])"
+        } 
+    }
+}
+
 Get-ChildItem -Recurse packages.config -Path $PSScriptRoot | foreach-object {
-    $newVersionString = 'package id="Microsoft.WindowsAppSDK" version="' + $WinAppSDKVersion + '"'
-    $oldVersionString = 'package id="Microsoft.WindowsAppSDK" version="[-.0-9a-zA-Z]*"'
     $content = Get-Content $_.FullName -Raw
-    $content = $content -replace $oldVersionString, $newVersionString
+
+    foreach ($nugetPackageToVersion in $nugetPackageToVersionTable.GetEnumerator())
+    {
+        $newVersionString = 'package id="' + $nugetPackageToVersion.Key + '" version="' + $nugetPackageToVersion.Value + '"'
+        $oldVersionString = 'package id="' + $nugetPackageToVersion.Key + '" version="[-.0-9a-zA-Z]*"'
+        $content = $content -replace $oldVersionString, $newVersionString
+    }
+
     Set-Content -Path $_.FullName -Value $content
     Write-Host "Modified " $_.FullName 
 }
 
 Get-ChildItem -Recurse *.vcxproj -Path $PSScriptRoot | foreach-object {
-    $newVersionString = '\Microsoft.WindowsAppSDK.' + $WinAppSDKVersion + '\'
-    $oldVersionString = '\\Microsoft.WindowsAppSDK.[-.0-9a-zA-Z]*\\'
     $content = Get-Content $_.FullName -Raw
-    $content = $content -replace $oldVersionString, $newVersionString
+
+    foreach ($nugetPackageToVersion in $nugetPackageToVersionTable.GetEnumerator())
+    {
+        $newVersionString = "\$($nugetPackageToVersion.Key)." + $nugetPackageToVersion.Value + '\'
+        $oldVersionString = "\\$($nugetPackageToVersion.Key).[0-9][-.0-9a-zA-Z]*\\"
+        $content = $content -replace $oldVersionString, $newVersionString
+    }
+
     Set-Content -Path $_.FullName -Value $content
     Write-Host "Modified " $_.FullName 
 }
