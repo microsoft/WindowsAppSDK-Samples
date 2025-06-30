b4b905e8f272689a4e8ccf9fd26efd00be5d51f7
Feng Wang <wangfen@microsoft.com>
2025-06-30 15:05:53 +0800

    update copyright info

--- Samples/WindowsML/python/SqueezeNetPython/main.py
+++ Samples/WindowsML/python/SqueezeNetPython/main.py
@@ -1,5 +1,5 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE.md in the repo root for license information.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 from pathlib import Path
 from PIL import Image
