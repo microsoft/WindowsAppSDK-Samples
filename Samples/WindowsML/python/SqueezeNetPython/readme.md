ad3b273457325c785d7df3a0c07eb90abd5d3faa
Feng Wang <wang6922@outlook.com>
2025-07-08 11:52:30 +0800

    Add the python example (#482)
    * Add the python example
    
    * update readme
    
    * update copyright info
    
    * udpate winui3 packages names. Version still TBD
    
    * set the correct WASDK version number
    
    ---------
    
    Co-authored-by: Feng Wang <wangfen@microsoft.com>

---
+++ Samples/WindowsML/python/SqueezeNetPython/readme.md
@@ -0,0 +1,24 @@
+---
+page_type: sample
+languages:
+- python
+products:
+- windows-app-sdk
+name: "WindowsML python"
+urlFragment: WindowsMLPython
+description: "Shows how to use WindowsML and ONNX Runtime to write python machine learning projects that runs across Windows AI hardware."
+extendedZipContent:
+- path: LICENSE
+  target: LICENSE
+---
+
+## Prerequisites
+* Python 3.10 - 3.13 on Windows
+* Download model file
+```PowerShell
+..\Download-Model.ps1
+```
+* Install dependencies
+```PowerShell
+pip install -r requirements.txt
+```
---
+++ Samples/WindowsML/python/SqueezeNetPython/winml_worker.py
@@ -0,0 +1,25 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import json
+
+def _get_ep_paths() -> dict[str, str]:
+    from winui3.microsoft.windows.applicationmodel.dynamicdependency.bootstrap import (
+        InitializeOptions,
+        initialize
+    )
+    import winui3.microsoft.windows.ai.machinelearning as winml
+    eps = {}
+    with initialize(options = InitializeOptions.ON_NO_MATCH_SHOW_UI):
+        pass
+        catalog = winml.ExecutionProviderCatalog.get_default()
+        providers = catalog.find_all_providers()
+        for provider in providers:
+            provider.ensure_ready_async().get()
+            eps[provider.name] = provider.library_path
+            # DO NOT call provider.try_register in python. That will register to the native env.
+    return eps
+
+if __name__ == "__main__":
+    eps = _get_ep_paths()
+    print(json.dumps(eps))
