c9708f949146bb2c3be63cc809e6f4de0bf6d1c0
Rui Marinho <me@ruimarinho.net>
2025-08-05 00:35:24 +0100

    Add wpf islands example

---
+++ Samples/Islands/SimpleWpfXamlIsland/SimpleWpfXamlIslandApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.UI.Dispatching;
+using SimpleWpfXamlIslandApp;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherQueueController _controller;
+        WinUIControlHost _winUIControl;
+
+        public MainWindow()
+        {
+            _controller = Microsoft.UI.Dispatching.DispatcherQueueController.CreateOnCurrentThread();
+
+            var xamlApp = new XamlApp();
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _winUIControl = new WinUIControlHost(ControlHostElement.ActualHeight, ControlHostElement.ActualWidth);
+            ControlHostElement.Child = _winUIControl;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello from WPF!");
+        }
+    }
+}
---
+++ Samples/Islands/SimpleWpfXamlIsland/SimpleWpfXamlIslandApp/WinUIControlHost.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using Windows.UI;
+using WinUILib;
+
+namespace SimpleWpfXamlIslandApp
+{
+    public class WinUIControlHost : HwndHost
+    {
+        int hostHeight, hostWidth;
+
+        Microsoft.UI.Xaml.Hosting.DesktopWindowXamlSource _xamlSource;
+
+
+        public WinUIControlHost(double height, double width)
+        {
+            hostHeight = (int)height;
+            hostWidth = (int)width;
+            _xamlSource = new Microsoft.UI.Xaml.Hosting.DesktopWindowXamlSource();
+        }
+
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            var id = new Microsoft.UI.WindowId((ulong)hwndParent.Handle);
+            _xamlSource.Initialize(id);
+
+            InitIslandSampleCode();
+            //_xamlSource.SiteBridge.ResizePolicy = Microsoft.UI.Content.ContentSizePolicy.ResizeContentToParentWindow;
+            //_xamlSource.SiteBridge.Show();
+
+            return new HandleRef(null, (nint)_xamlSource.SiteBridge.WindowId.Value);
+        }
+
+        void InitIslandSampleCode()
+        {
+            var grid = new Microsoft.UI.Xaml.Controls.Grid();
+            grid.Background = new Microsoft.UI.Xaml.Media.SolidColorBrush(Colors.LightGray);
+            grid.RowDefinitions.Add(new Microsoft.UI.Xaml.Controls.RowDefinition { Height = new Microsoft.UI.Xaml.GridLength(1, Microsoft.UI.Xaml.GridUnitType.Star) });
+            grid.RowDefinitions.Add(new Microsoft.UI.Xaml.Controls.RowDefinition { Height = new Microsoft.UI.Xaml.GridLength(1, Microsoft.UI.Xaml.GridUnitType.Star) });
+
+            var winUiButton = new MyCustomButton();
+            winUiButton.Click += (s, e) =>
+            {
+                System.Windows.MessageBox.Show("Hello from WinUI!");
+            };
+            grid.Children.Add(winUiButton);
+
+            var listView = new CustomListView {
+
+                ItemsSource = new List<string> { "Item 1", "Item 2", "Item 3" },
+            };
+
+            Microsoft.UI.Xaml.Controls.Grid.SetRow(listView, 1);
+            grid.Children.Add(listView);
+
+            _xamlSource.Content = grid;
+        }
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+
+        }
+    }
+}
