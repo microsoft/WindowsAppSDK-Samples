755a83fe71586cd753a5f54b39a2398d1cd0a144
Dinah Gao (from Dev Box) <xiaomgao@microsoft.com>
2025-09-24 13:56:55 +0800

    windows size; graceful

--- Samples/StoragePickers/cs-sample/MainWindow.xaml.cs
+++ Samples/StoragePickers/cs-sample/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Text.Json.Serialization;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -33,6 +23,9 @@ namespace FilePickersAppSinglePackaged
         {
             this.InitializeComponent();
             Title = "File Pickers Sample App";
+            
+            // Set window size to make it taller
+            this.AppWindow.Resize(new Windows.Graphics.SizeInt32(1800, 1100));
         }
 
         #region Helper Methods
