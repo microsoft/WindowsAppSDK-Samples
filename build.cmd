46679a4c91644792526a3ebfe324a2cb951c11ba
Aditya Rastogi <adityar@ntdev.microsoft.com>
2025-10-06 16:10:08 -0700

    PR feedback (tightening output)

--- build.cmd
+++ build.cmd
@@ -102,7 +102,7 @@ if exist "%VS_INSTALL_PATH%\Common7\Tools\VsDevCmd.bat" (
 :EnsureDeveloperCommandPromptEnd
 if "%VSINSTALLDIR%"=="" (
     echo Failed to automatically configure the Visual Studio environment.
-    goto :usage
+    echo Please manually run it from a Developer Command Prompt instead.
 )
 goto :eof
 
