081a0f44de4aa83da4982e841e016779600907f4
shisan <coco13_sim@163.com>
2025-10-28 14:41:23 +0800

    fix: resolve C4100 unreferenced parameter warnings across samples (#513)
    * fix: resolve C4100 unreferenced parameter warnings across samples
    
    * set C4100 treateAsError
    
    * move 8305 suppress to global
    
    * extra C4100 process
    
    * add extra test
    
    * ext
    
    ---------
    
    Co-authored-by: Qiutong Shen <qiutongshen@microsoft.com>

--- Samples/TextRendering/cpp-win32/DWriteCoreGallery/Main.cpp
+++ Samples/TextRendering/cpp-win32/DWriteCoreGallery/Main.cpp
@@ -18,8 +18,8 @@ _Ret_z_ WCHAR const* GetAppTitle()
 
 int APIENTRY wWinMain(
     _In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR lpCmdLine,
+    _In_opt_ HINSTANCE /*hPrevInstance*/,
+    _In_ LPWSTR /*lpCmdLine*/,
     _In_ int nCmdShow
 )
 {
