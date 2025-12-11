3babea3aea8e103ed35ec36840b14037f90569c6
Aditya Rastogi <adityar@ntdev.microsoft.com>
2025-12-11 12:57:44 -0800

    Refactor

---
+++ Samples/WindowsML/python/SqueezeNetPython/Install-Requirements.ps1
@@ -0,0 +1,7 @@
+# Install dependencies for SqueezeNetPython sample
+
+# Install onnxruntime-winml from ORT-Nightly feed
+pip install --upgrade --pre -r requirements-ort.txt
+
+# Install remaining dependencies from PyPI
+pip install --upgrade --pre -r requirements.txt
