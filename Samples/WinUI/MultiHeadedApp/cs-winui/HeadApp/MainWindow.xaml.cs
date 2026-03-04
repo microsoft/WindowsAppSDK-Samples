2f8f0481ef560c06166ac3112fcc3755710fada4
guimafelipe <guima.felipec@gmail.com>
2026-03-04 14:31:01 -0800

    Add multi-headed MSIX sample with two WinUI apps

---
+++ Samples/WinUI/MultiHeadedApp/cs-winui/HeadApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace HeadApp
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Samples/WinUI/MultiHeadedApp/cs-winui/SecondaryApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml;
+
+namespace SecondaryApp
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
