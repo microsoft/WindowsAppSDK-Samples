fb7fcf1ef2d9bdb0872f6d7d19e4fe48ef270c35
guimafelipe <guima.felipec@gmail.com>
2026-03-04 15:40:03 -0800

    Add C++ multi-headed MSIX sample

---
+++ Samples/WinUI/MultiHeadedApp/cpp-winui/CppHeadApp/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <windows.h>
+#include <unknwn.h>
+#include <restrictederrorinfo.h>
+#include <hstring.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Microsoft.UI.Xaml.Markup.h>
+#include <winrt/Microsoft.UI.Xaml.Navigation.h>
---
+++ Samples/WinUI/MultiHeadedApp/cpp-winui/CppSecondaryApp/MainWindow.xaml.cpp
@@ -0,0 +1,5 @@
+#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
